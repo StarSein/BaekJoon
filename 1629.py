2f172e53404a7565384237d07c2c49dd06ac6e84
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2021-06-17 15:07:35 +0900

    Update 1629.py
    AC !! in Python3
    pow 연산의 시간복잡도가 엄청나다고 들은 것 같은데.. 이게 되네??

--- 1629.py
+++ 1629.py
@@ -2,17 +2,4 @@ import sys
 
 
 A, B, C = map(int, sys.stdin.readline().split())
-mod = A % C
-result = mod ** B
-if B != 1:
-    i = 2
-    l_mod = [mod]
-    while i <= B:
-        mod = mod ** 2 % C
-        l_mod.append(mod)
-        i *= 2
-    result = 1
-    str_binB = str(bin(B))
-    for j in filter(lambda x: str_binB[x] == '1', range(2, len(str_binB))):
-        result = result * l_mod[len(str_binB) - 1 - j] % C
-print(result)
+print(pow(A, B, C))
