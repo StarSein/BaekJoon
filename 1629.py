7698e9712a4877423915ec317d4d4f6fe29071da
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2021-06-17 15:54:10 +0900

    Update 1629.py
    AC !! in Python3. totally same spend of time and memory with the latest source code.
    https://pythonq.com/so/python/538245
    이 페이지에서 배웠다.
    비트연산이라서 더 빠른 것 같다. 정수의 계산 속도를 개선하기 위해 비트연산의 사용도 고려해 봐야 겠다.
    그리고int형 자료형의 범위 안에서는 계산값의 크기가 소요시간에 영향을 주지 않는 것 같다. 따라서 일부러 크기가 작은 수들의 연산을 하려고 계산을 세분화할 필요가 없다. 오히려 연산의 수가 늘어나 프로그램 종료까지 더 오래 걸린다.

--- 1629.py
+++ 1629.py
@@ -1,5 +1,15 @@
 import sys
 
 
+def fast_mod_exp(b, exp, m):
+    result = 1
+    while exp > 1:
+        if exp & 1:
+            result = (result * b) % m
+        b = b ** 2 % m
+        exp >>= 1
+    return (b * result) % m
+
+
 A, B, C = map(int, sys.stdin.readline().split())
-print(pow(A, B, C))
+print(fast_mod_exp(A, B, C))
