110942577ed69008be32a18119590c9751ee3252
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2021-06-19 01:48:20 +0900

    Update 15650.py
    dfs 함수의 출력부를 제외한 loop에서 백트래킹의 기능을 수행하는 코드는 line 18~19와 그것을 보조하는 line 21, 26~27, 깊이 우선 탐색의 기능을 수행하는 코드는 line 23~24이다.

--- 15650.py
+++ 15650.py
@@ -3,29 +3,28 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
-num_list = [i + 1 for i in range(N)]
-check_list = [False] * N
+num_list = [i + 1 for i in range(N)]    # index 에 숫자와 그 숫자가 수열을 구성하고 있는지의 여부를 대응시킨다.
+check_list = [False] * N                # 따라서 숫자를 넣을지 말지 결정할 때 index 를 매개로 하여 현재 상태를 확인 가능하다.
+arr = []                                # 수열이 담길 배열 혹은 스택 arr
 
-arr = []
 
+def dfs(count):                         # 재귀함수 dfs 의 중첩단계와 같은 변수 count 를 매개변수로 하는 깊이 우선 탐색 함수
 
-def dfs(cnt):
+    if count == M:                      # count 가 0 이라면, 현재 배열 arr 에는 M 개의 원소가 있음
+        print(*arr)                     # 따라서 asterisk 로 배열 arr 을 unpacking 하여 출력함
+        return                          # 함수를 종료함 (현재의 깊이 우선 탐색의 한 줄기가 더 이상 뻗어나가지 않고, 다음 줄기로 넘어감)
 
-    if cnt == M:
-        print(*arr)
-        return
-
-    for i in range(N):
+    for i in range(N):                  # 배열 arr 의 새 원소로 넣을 수 있는 숫자에 대해서만 line 21~27 의 코드를 실행함
         if check_list[i]:
             continue
 
-        check_list[i] = True
-        arr.append(num_list[i])
-        dfs(cnt + 1)
-        arr.pop()
+        check_list[i] = True            # i 라는 index 에 대응되는 숫자가 현재의 탐색 줄기에서 더 이상 사용되지 않도록 하고,
+        arr.append(num_list[i])         # i 라는 index 에 대응되는 숫자를 배열 arr 에 추가함 ( 이는 배열 arr 에서 count 번째 원소)
+        dfs(count + 1)                  # 현재의 배열 arr 을 유지한 채 현재의 깊이 우선 탐색 줄기를 한 칸 더 뻗어나감
+        arr.pop()                       # 다음 loop 에서 배열 arr 의 count 번째 항목으로 그 다음 숫자가 할당될 수 있도록 지금 숫자를 삭제함
 
-        for j in range(i + 1, N):
-            check_list[j] = False
+        for j in range(i + 1, N):       # i 라는 index 에 대응되는 숫자가 배열 arr 의 마지막 항목으로 존재했는데,
+            check_list[j] = False       # 그 이후의 숫자들이 다음 깊이 우선 탐색에서 사용될 수 있도록, 그 숫자들과 대응되는 bool 을 False 로 할당함
+                                       
 
-
-dfs(0)
+dfs(0)                                  # 배열 arr 의 0번째 항목부터 너비 우선 탐색, 그런데 이제 백트래킹을 곁들인... 을 시작함
