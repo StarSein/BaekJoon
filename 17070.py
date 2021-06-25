b06f16dc23b511b98e25b6472fd70f979b0c5f3e
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2021-06-25 12:33:19 +0900

    Update 17070.py
    AC !! in Python 3
    딕셔너리의 key로 튜플형 상수가 올 수 있는 것이 신기했다. 그에 대응한 value로 '동일한 좌표와 진행방향'에 해당하는 경우의 수(count)를 저장했다. 함수들이 매개변수를 3개를 받게 되면서 코드가 더 이해하기 어렵게 된 것 같기도 하지만, 함수형 프로그래밍 느낌으로 작성한 코드 덕에 내가 구상한 알고리즘의 접근 방식이 구조적, 체계적으로 이해된다.

--- 17070.py
+++ 17070.py
@@ -1,68 +1,77 @@
 import sys
 
 
-def pipe_move3(col, row):
-    move_right(col, row)
-    move_diagonal(col, row)
+def pipe_move3(col, row, count):
+    move_right(col, row, count)
+    move_diagonal(col, row, count)
 
 
-def pipe_move4(col, row):
-    move_right(col, row)
-    move_diagonal(col, row)
-    move_down(col, row)
+def pipe_move4(col, row, count):
+    move_right(col, row, count)
+    move_diagonal(col, row, count)
+    move_down(col, row, count)
 
 
-def pipe_move6(col, row):
-    move_diagonal(col, row)
-    move_down(col, row)
+def pipe_move6(col, row, count):
+    move_diagonal(col, row, count)
+    move_down(col, row, count)
 
 
-def move_down(col, row):
-    global count
+def move_down(col, row, count):
+    global result
     if col + 1 < N and not matrix[col + 1][row]:
         if col + 1 == N - 1 and row == N - 1:
-            count += 1
+            result += count
         elif col + 1 < N - 1:
-            dp[-1].append((col + 1, row, 6))
+            if (col + 1, row, 6) in dp[-1]:
+                dp[-1][(col + 1, row, 6)] += count
+            else:
+                dp[-1][(col + 1, row, 6)] = count
         else:
             pass
 
 
-def move_diagonal(col, row):
-    global count
+def move_diagonal(col, row, count):
+    global result
     if col + 1 < N and row + 1 < N and not (matrix[col + 1][row + 1] or matrix[col + 1][row] or matrix[col][row + 1]):
         if col + 1 == N - 1 and row + 1 == N - 1:
-            count += 1
+            result += count
         elif col + 1 <= N - 1 and row + 1 <= N - 1:
-            dp[-1].append((col + 1, row + 1, 4))
+            if (col + 1, row + 1, 4) in dp[-1]:
+                dp[-1][(col + 1, row + 1, 4)] += count
+            else:
+                dp[-1][(col + 1, row + 1, 4)] = count
         else:
             pass
 
 
-def move_right(col, row):
-    global count
+def move_right(col, row, count):
+    global result
     if row + 1 < N and not matrix[col][row + 1]:
         if row + 1 == N - 1 and col == N - 1:
-            count += 1
+            result += count
         elif row + 1 < N - 1:
-            dp[-1].append((col, row + 1, 3))
+            if (col, row + 1, 3) in dp[-1]:
+                dp[-1][(col, row + 1, 3)] += count
+            else:
+                dp[-1][(col, row + 1, 3)] = count
         else:
             pass
 
 
 N = int(sys.stdin.readline())
 matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-dp = [[(0, 1, 3)]]
-count = 0
+dp = [{(0, 1, 3): 1}]
+result = 0
 while True:
-    dp.append([])
+    dp.append(dict())
     for item in dp[-2]:
         if item[2] == 3:
-            pipe_move3(item[0], item[1])
+            pipe_move3(item[0], item[1], dp[-2][item])
         elif item[2] == 6:
-            pipe_move6(item[0], item[1])
+            pipe_move6(item[0], item[1], dp[-2][item])
         else:
-            pipe_move4(item[0], item[1])
+            pipe_move4(item[0], item[1], dp[-2][item])
     if len(dp[-1]) == 0:
         break
-print(count)
+print(result)
