17990e9188a8d30ceaf34af04a536d6ec219d811
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2021-06-28 13:53:05 +0900

    Add files via upload
    TLE in Python 3
    AC !! in PyPy3
    134292KB / 128MB, 6064ms / 10s
    
    처음에는 이중 배열을 그대로 구현하여, 설치한 퀸의 공격범위를 모두 False 처리하려고 했다. 그런데 그러려니 이중 배열을 모두 순회하여 조건에 맞는 index의 value를 변경하는 작업 때문에 시간 복잡도도 늘어나고, 백트래킹의 과정에서 그 변경 사항을 다시 되돌리는 작업도 필요한데 그러려면 변경했던 index들이 무엇인지 따로 임시 배열을 만들어 저장해놔야 했고 그러려면 공간 복잡도도 늘어나며, 재귀가 중첩이 되는 와중에 어떻게 임시 변수들을 관리할지도 감이 안 잡혔다.
    그러던 중 요즈음 읽고 있는 "알고리즘 트레이닝"(안티 라크소넨)을 통해 N-Queen 문제를 어떻게 다루는지 배울 수 있었고, 그 풀이법을 Python으로 구현해 봤다. 공격범위에 있는지 여부를 확인하는 배열을 3개를 만들어, '순회-조건 탐색'이 아닌 '접근-조건 확인'이라는 훨씬 최적화된 방법으로 백트래킹을 한다는 점이 참신했다.

--- 9663.py
+++ 9663.py
@@ -1,50 +1,23 @@
 import sys
-from math import perm
+
+
+def search(k):
+    global cnt
+    if k == N:
+        cnt += 1
+    else:
+        for i in range(N):
+            if col[i] | diag1[i + k] | diag2[i - k + N - 1]:
+                continue
+            col[i] = diag1[i + k] = diag2[i - k + N - 1] = True
+            search(k + 1)
+            col[i] = diag1[i + k] = diag2[i - k + N - 1] = False
 
 
 N = int(sys.stdin.readline())
-num_case = 0
-case = []
-ll_check = [[False] * N] * N
-
-
-def dfs(y, x, count):
-    global num_case, ll_check
-    if count == N:
-        num_case += 1
-        return
-
-    for j in range(N):
-        for i in range(N):
-            if ll_check[j][i]:
-                continue
-
-            for p in range(N):
-                ll_check[j][p] = True
-            for q in range(N):
-                ll_check[q][i] = True
-            for b in range(N):
-                for a in range(N):
-                    if b + a == j + i or b - a == j - i:
-                        ll_check[b][a] = True
-            case.append((j, i))
-            dfs(y, x, count + 1)
-            for p in range(N):
-                ll_check[j][p] = False
-            for q in range(N):
-                ll_check[q][i] = False
-            for b in range(N):
-                for a in range(N):
-                    if b + a == j + i or b - a == j - i:
-                        ll_check[b][a] = False
-            case.pop()
-
-
-for n in range(N):
-    for m in range(N):
-        dfs(n, m, 0)
-
-if num_case:
-    print(num_case//perm(num_case, N))
-else:
-    print(0)
+col = [False] * N
+diag1 = [False] * (2 * N - 1)
+diag2 = [False] * (2 * N - 1)
+cnt = 0
+search(0)
+print(cnt)
