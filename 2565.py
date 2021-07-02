086d413317aa548b026169d3277feda523c06177
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2021-07-03 01:12:30 +0900

    Add files via upload
    AC !! in Python 3
    이번에는 LIS를 구하는 다른 한 방법인 동적 계획법을 통해 풀이해 봤다. line 15~21까지의 코드를 통해 쓸모없는 케이스들을 버리고 가는 것이 시스템 낭비를 막기 위해 필요하다고 판단했다.

--- 2565.py
+++ 2565.py
@@ -1,32 +1,23 @@
 import sys
 
 
-def lower_bound(start, end, n):
-    if vector[end] < n:
-        vector.append(n)
-    elif vector[start] > n:
-        vector[start] = n
-    else:
-        if start == end:
-            return 0
-        elif start == end - 1:
-            if vector[start] != n:
-                vector[end] = n
-        else:
-            mid = (start + end) // 2
-            if vector[mid] < n:
-                start = mid
-            elif vector[mid] > n:
-                end = mid
-            else:
-                return 0
-            return lower_bound(start, end, n)
-
-
 N = int(sys.stdin.readline())
 l_cable = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
 l_cable.sort()
-vector = [l_cable[0][1]]
+dp = [[] for _ in range(N)]
+dp[0] = [(l_cable[0][1], 1), (0, 0)]
 for idx in range(1, N):
-    lower_bound(0, len(vector) - 1, l_cable[idx][1])
-print(N - len(vector))
+    for item in dp[idx-1]:
+        if item[0] < l_cable[idx][1]:
+            dp[idx].append((l_cable[idx][1], item[1] + 1))
+        dp[idx].append(item)
+    dp[idx].sort(key=lambda x: (-x[1], x[0]))
+    useful = [dp[idx][0]]
+    length = dp[idx][0][1]
+    for item_ in dp[idx]:
+        if item_[1] < length:
+            useful.append(item_)
+            length = item_[1]
+    dp[idx] = useful
+LIS_len = dp[N-1][0][1]
+print(N - LIS_len)
