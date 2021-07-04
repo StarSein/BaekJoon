86bc1b9b557cd3f001d6af1dda39295211553d4b
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2021-07-04 23:25:24 +0900

    Update 14889.py
    AC !! in Python 3, PyPy3
    Python 3 : 29200KB / 512MB, 4652ms / 2s
    PyPy3 : 125576KB / 512MB, 832ms / 2s
    이 코드에서는 itertools 모듈의 combinations 함수를 통해 team1에 일괄적으로 인원을 배분하고, 백트래킹을 적용하여 team2에 나머지 인원을 배분했다.
    team1, 2 각각에 대해 팀원을 한 명씩 뽑는 재귀함수와 함께 백트래킹을 구현할 때보다 시간 복잡도가 더욱(내 예상에는 두 배 이상) 개선된 모습이다.

--- 14889.py
+++ 14889.py
@@ -2,9 +2,15 @@ import sys
 from itertools import combinations
 
 
-def allocate(k):
+def allocate():
     global min_gap
-    if k == N//2:
+    for team1 in combinations(range(N), N//2):
+        for member in team1:
+            l_check[member] = True
+        team2 = []
+        for person in range(N):
+            if not l_check[person]:
+                team2.append(person)
         score1, score2 = 0, 0
         for item1 in combinations(team1, 2):
             score1 += S[item1[0]][item1[1]] + S[item1[1]][item1[0]]
@@ -12,27 +18,13 @@ def allocate(k):
             score2 += S[item2[0]][item2[1]] + S[item2[1]][item2[0]]
         gap = abs(score1 - score2)
         min_gap = min(min_gap, gap)
-    for i in range(N):
-        if l_check[i]:
-            continue
-        l_check[i] = True
-        team1.append(i)
-        for j in range(N):
-            if l_check[j]:
-                continue
-            l_check[j] = True
-            team2.append(j)
-            allocate(k + 1)
-            l_check[j] = False
-            team2.pop()
-        l_check[i] = False
-        team1.pop()
+        for member in team1:
+            l_check[member] = False
 
 
 N = int(sys.stdin.readline())
 S = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 l_check = [False] * N
-team1, team2 = [], []
 min_gap = sys.maxsize
-allocate(0)
+allocate()
 print(min_gap)
