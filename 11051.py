63107274bd527583967c9e500fa8fd3f8f5ff17c
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2021-07-07 22:51:55 +0900

    Add files via upload
    RE(Runtime Error) in Python 3
    
    k = 0일 때 d_gcd = 0이 되고, line 12와 line 15에서 0으로 나누는 연산을 하게 되면서 RE가 발생한다.

---
+++ 11051.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def comb(n, k):
+    upside = n
+    downside = k
+    n -= 1
+    k -= 1
+    while k:
+        u_gcd = euclid(upside, k)
+        d_gcd = euclid(downside, n)
+        upside *= n // d_gcd
+        upside //= u_gcd
+        downside *= k // u_gcd
+        downside //= d_gcd
+        n -= 1
+        k -= 1
+    return upside // downside
+
+
+def euclid(a, b):
+    if b == 0:
+        return a
+    else:
+        return euclid(b, a % b)
+
+
+N, K = map(int, sys.stdin.readline().split())
+print(comb(N, K) % 10007)
