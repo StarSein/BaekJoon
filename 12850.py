aa55710e0f100bf150ed40b48bfe44cb50f2cc36
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2021-07-16 19:37:57 +0900

    Update 12850.py
    AC !! in Python 3
    
    출발 및 도착 노드 또한 중간에 방문하는 것이 가능하다는 조건 아래에서 모든 경로의 수를 구하는 문제였다. 샘플 데이터와 다른 결과값이 나와 찾아보니 line 16~26의 반복문을 빠져나오게 되면 필연적으로 k의 값은 D의 값보다 클 수밖에 없었고, 그래서 logical error에 직면했던 것이었다. k의 값을 다시 2로 나눠줌으로써 오류를 바로잡을 수 있었다.

--- 12850.py
+++ 12850.py
@@ -1,42 +1,41 @@
 import sys
 
 
-D = int(sys.stdin.readline()) - 2
-if D < 0:
-    print(0)
-    exit(0)
-matrix = [[] for _ in range(7)]  # 전, 미, 신, 한, 진, 형, 학
-matrix[0] = [0, 1, 1, 0, 0, 0, 0]
-matrix[1] = [1, 0, 1, 1, 0, 0, 0]
-matrix[2] = [1, 1, 0, 1, 1, 0, 0]
-matrix[3] = [0, 1, 1, 0, 1, 1, 0]
-matrix[4] = [0, 0, 1, 1, 0, 0, 1]
-matrix[5] = [0, 0, 0, 1, 0, 0, 1]
-matrix[6] = [0, 0, 0, 0, 1, 1, 0]
-
+D = int(sys.stdin.readline())
+matrix = [[] for _ in range(8)]  # 정, 전, 미, 신, 한, 진, 형, 학
+matrix[0] = [0, 1, 1, 0, 0, 0, 0, 0]
+matrix[1] = [1, 0, 1, 1, 0, 0, 0, 0]
+matrix[2] = [1, 1, 0, 1, 1, 0, 0, 0]
+matrix[3] = [0, 1, 1, 0, 1, 1, 0, 0]
+matrix[4] = [0, 0, 1, 1, 0, 1, 1, 0]
+matrix[5] = [0, 0, 0, 1, 1, 0, 0, 1]
+matrix[6] = [0, 0, 0, 0, 1, 0, 0, 1]
+matrix[7] = [0, 0, 0, 0, 0, 1, 1, 0]
+matrix_1 = matrix
 k = 2
 while k <= D:
-    temp = [[] for _ in range(7)]
-    for c in range(7):
-        for r in range(7):
+    temp = [[] for _ in range(8)]
+    for c in range(8):
+        for r in range(8):
             prd = 0
-            for t in range(7):
+            for t in range(8):
                 prd += (matrix[c][t] * matrix[t][r]) % 1000000007
             temp[c].append(prd % 1000000007)
     matrix = temp
     globals()['matrix_{}'.format(k)] = temp
     k <<= 1
+k >>= 1
 D -= k
 while D > 0:
     k >>= 1
     if D >= k:
-        temp = [[] for _ in range(7)]
-        for c in range(7):
-            for r in range(7):
+        temp = [[] for _ in range(8)]
+        for c in range(8):
+            for r in range(8):
                 prd = 0
-                for t in range(7):
+                for t in range(8):
                     prd += (matrix[c][t] * globals()['matrix_{}'.format(k)][t][r]) % 1000000007
                 temp[c].append(prd % 1000000007)
         matrix = temp
         D -= k
-print((matrix[0][0] + matrix[0][1] + matrix[1][0] + matrix[1][1]) % 1000000007)
+print(matrix[0][0])
