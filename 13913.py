8ef3eb0907b4c90c76edc54f67cb16e44631af71
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2021-07-17 23:45:57 +0900

    Add files via upload
    WA in Python 3
    
    line 5~6의 코드가 기능하는 경우(N >= K인 경우) 출력값이 출력양식에 어긋난다.

--- 13913.py
+++ 13913.py
@@ -5,23 +5,24 @@ N, K = map(int, sys.stdin.readline().split())
 if N >= K:
     print(N - K)
 else:
-    dp = [[(N, [N])]]
+    dp0 = [(N, [N])]
     time = 1
     set_visit = set()
     set_visit.add(N)
     det = True
     while det:
-        dp.append([])
-        for item in dp[-2]:
+        dp1 = []
+        for item in dp0:
             for new_num in [item[0] - 1, item[0] + 1, item[0] * 2]:
                 if new_num not in set_visit and 0 <= new_num < K * 2:
                     if new_num == K:
                         det = False
                     else:
                         set_visit.add(new_num)
-                        dp[-1].append((new_num, item[1] + [new_num]))
+                        dp1.append((new_num, item[1] + [new_num]))
             if not det:
                 print(time)
                 print(*item[1], K)
                 break
         time += 1
+        dp0 = dp1
