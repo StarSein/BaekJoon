d616107cafb399143fb98c7cd1e8108e69993d2e
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2021-07-17 23:28:18 +0900

    Add files via upload
    MLE in Python 3
    
    새로 이동하는 지점이 터무니 없이 큰 수일 때도 더 이상 고려하지 않도록 조건을 두어야 더욱 개선될 것이다.

--- 13913.py
+++ 13913.py
@@ -7,14 +7,21 @@ if N >= K:
 else:
     dp = [[(N, [N])]]
     time = 1
-    while True:
+    set_visit = set()
+    set_visit.add(N)
+    det = True
+    while det:
         dp.append([])
         for item in dp[-2]:
-            if item[0] - 1 == K or item[0] + 1 == K or item[0] * 2 == K:
+            for new_num in [item[0] - 1, item[0] + 1, item[0] * 2]:
+                if new_num not in set_visit:
+                    if new_num == K:
+                        det = False
+                    else:
+                        set_visit.add(new_num)
+                        dp[-1].append((new_num, item[1] + [new_num]))
+            if not det:
                 print(time)
                 print(*item[1], K)
-                exit(0)
-            dp[-1].append((item[0] - 1, item[1] + [item[0] - 1]))
-            dp[-1].append((item[0] + 1, item[1] + [item[0] + 1]))
-            dp[-1].append((item[0] * 2, item[1] + [item[0] * 2]))
+                break
         time += 1
