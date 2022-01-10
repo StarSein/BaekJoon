2066627ef1a458e282376d73fadf948fc062a47d
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-11 08:39:18 +0900

    19951.2.1
    AC
    
    이전 풀이가 O(NlogN)이었다면, 이 풀이는 O(N)이다.
    x좌표값에 대한 iteration이 요구되는 문제이므로, 굳이 heapq을 사용할 필요가 없었다.
    각 x좌표에 대해 적용할 변화량을 누적합으로 구하면 되는 것이었다.

--- 19951.py
+++ 19951.py
@@ -1,38 +1,26 @@
 import sys
-import heapq
-from typing import List, Tuple
+from typing import List
 
 
 input = sys.stdin.readline
 
 
-def solution(n: int, default_heights: List[int], cmd_list: List[Tuple[int]]) -> List[str]:
-    final_heights = []
-    pos_heap = []
-    for idx, val in enumerate(cmd_list):
-        start_pos, end_pos, k = val[0], val[1], val[2]
-        heapq.heappush(pos_heap, (start_pos, k))
-        heapq.heappush(pos_heap, (end_pos + 1, -k))
-    next_cmd = heapq.heappop(pos_heap)
+def solution(default_h: List[int], change_h: List[int]) -> List[str]:
+    result_h = [str(default_h[0] + change_h[0])]
+    for i in range(1, len(change_h) - 1):
+        change_h[i] += change_h[i-1]
+        result_h.append(str(default_h[i] + change_h[i]))
 
-    amount_change = 0
-    for i in range(1, n+1):
-        while i == next_cmd[0]:
-            amount_change += next_cmd[1]
-            if len(pos_heap):
-                next_cmd = heapq.heappop(pos_heap)
-            else:
-                break
-        final_heights.append(str(default_heights[i-1] + amount_change))
-
-    return final_heights
+    return result_h
 
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    default_heights = list(map(int, input().split()))
-    cmd_list = []
+    default_h = list(map(int, input().split()))
+    change_h = [0] * (n + 1)
     for i in range(m):
-        cmd_list.append(tuple(map(int, input().split())))
-    sol = solution(n, default_heights, cmd_list)
+        start_pos, end_pos, change = map(int, input().split())
+        change_h[start_pos-1] += change
+        change_h[end_pos] -= change
+    sol = solution(default_h, change_h)
     print(' '.join(sol))
