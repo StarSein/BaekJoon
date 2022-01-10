b476da7ec4c527fc35b3d76d188f44c067e55793
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-10 21:34:37 +0900

    19951.1.2
    AC !!!
    
    이전 코드의 경우, end_heap의 길이가 0이라는 조건을 통과한 이후로는, next_cover가 갱신되지 않으므로 절대 end_heap에 담긴 명령의 처리가 진행되지 않는다.

--- 19951.py
+++ 19951.py
@@ -6,25 +6,31 @@ from typing import List, Tuple
 input = sys.stdin.readline
 
 
-def solution(n: int, default_heights: List[int], start_heap: List[Tuple[int]]) -> List[str]:
+def solution(n: int, default_heights: List[int], cmd_list: List[Tuple[int]]) -> List[str]:
     final_heights = []
-    heapq.heapify(start_heap)
-    next_dig = heapq.heappop(start_heap)
+    start_heap = []
     end_heap = []
-    heapq.heappush(end_heap, (next_dig[1] + 1, next_dig[2]))
+    for idx, val in enumerate(cmd_list):
+        start_pos, end_pos, k = val[0], val[1], val[2]
+        heapq.heappush(start_heap, (start_pos, k))
+        heapq.heappush(end_heap, (end_pos + 1, k))
+    next_dig = heapq.heappop(start_heap)
     next_cover = heapq.heappop(end_heap)
+
     amount_change = 0
     for i in range(1, n+1):
         while i == next_dig[0]:
-            amount_change += next_dig[2]
+            amount_change += next_dig[1]
             if len(start_heap):
                 next_dig = heapq.heappop(start_heap)
-                heapq.heappush(end_heap, (next_dig[1] + 1, next_dig[2]))
             else:
                 break
         while i == next_cover[0]:
             amount_change -= next_cover[1]
-            next_cover = heapq.heappop(end_heap)
+            if len(end_heap):
+                next_cover = heapq.heappop(end_heap)
+            else:
+                break
         final_heights.append(str(default_heights[i-1] + amount_change))
 
     return final_heights
