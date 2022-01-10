22702b07a02653a0c1b4a5300c80fc7640f94f48
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-10 20:00:53 +0900

    6603.2.1
    AC
    
    확실히 표준 라이브러리를 사용하는 것이 훨씬 깔끔하다.

--- 6603.py
+++ 6603.py
@@ -1,31 +1,15 @@
 import sys
 from typing import List
+from itertools import combinations
 
 input = sys.stdin.readline
 NUM_LOTTO = 6
 
 
-def solution(elements: List[int]):
-    collected = []
-    latest_idx = -1
-    collect_nums(latest_idx, collected, elements)
-
-
-def collect_nums(latest_idx: int, collected: List[int], elements: List[int]):
-    if len(collected) == NUM_LOTTO:
-        for idx, val in enumerate(collected):
-            print(val, end=' ')
-        print()
-        return
-
-    for idx, val in enumerate(elements):
-        if idx > latest_idx:
-            collected.append(val)
-            tmp_idx = latest_idx
-            latest_idx = idx
-            collect_nums(latest_idx, collected, elements)
-            collected.pop()
-            latest_idx = tmp_idx
+def solution(elements: List[str]):
+    for case in combinations(elements, NUM_LOTTO):
+        print(' '.join(case))
+    print()
 
 
 if __name__ == '__main__':
@@ -34,6 +18,7 @@ if __name__ == '__main__':
         k = inp[0]
         if k == 0:
             break
-        elements = inp[1:]
+        elements = []
+        for i in range(1, k+1):
+            elements.append(str(inp[i]))
         solution(elements)
-        print()
