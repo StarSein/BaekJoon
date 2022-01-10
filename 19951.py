99f12f0dd83bd3398320774138b154d65199492a
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-10 21:43:28 +0900

    19951.1.3
    AC
    
    2개의 heapq을 쓰던 것을 1개로 줄이니 코드가 더욱 간결해졌다. 메모리와 시간 소요는 대동소이하게 늘어났다.

--- 19951.py
+++ 19951.py
@@ -8,27 +8,19 @@ input = sys.stdin.readline
 
 def solution(n: int, default_heights: List[int], cmd_list: List[Tuple[int]]) -> List[str]:
     final_heights = []
-    start_heap = []
-    end_heap = []
+    pos_heap = []
     for idx, val in enumerate(cmd_list):
         start_pos, end_pos, k = val[0], val[1], val[2]
-        heapq.heappush(start_heap, (start_pos, k))
-        heapq.heappush(end_heap, (end_pos + 1, k))
-    next_dig = heapq.heappop(start_heap)
-    next_cover = heapq.heappop(end_heap)
+        heapq.heappush(pos_heap, (start_pos, k))
+        heapq.heappush(pos_heap, (end_pos + 1, -k))
+    next_cmd = heapq.heappop(pos_heap)
 
     amount_change = 0
     for i in range(1, n+1):
-        while i == next_dig[0]:
-            amount_change += next_dig[1]
-            if len(start_heap):
-                next_dig = heapq.heappop(start_heap)
-            else:
-                break
-        while i == next_cover[0]:
-            amount_change -= next_cover[1]
-            if len(end_heap):
-                next_cover = heapq.heappop(end_heap)
+        while i == next_cmd[0]:
+            amount_change += next_cmd[1]
+            if len(pos_heap):
+                next_cmd = heapq.heappop(pos_heap)
             else:
                 break
         final_heights.append(str(default_heights[i-1] + amount_change))
