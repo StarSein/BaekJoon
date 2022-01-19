0a0196bcd1f028791535d15cb1a493831bbd5c7c
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-20 00:14:54 +0900

    2512.1.1
    AC !!!
    
    알고리즘 자체는 간단한데, 이진 탐색의 중단 조건을 찾는 로직을 떠올리는 데에 시간이 많이 들었다.

---
+++ 2512.py
@@ -0,0 +1,45 @@
+import sys
+
+
+input = sys.stdin.readline
+MAX_BUDGET = 100_000
+MIN_BUDGET = 1
+
+
+def solution() -> int:
+    if sum(budget_list) <= total_budget:
+        return max(budget_list)
+
+    sol = binary_search(MIN_BUDGET, MAX_BUDGET)
+    return sol
+
+
+def binary_search(start: int, end: int) -> int:
+    mid = (start + end) // 2
+    budget_sum = 0
+    for idx, val in enumerate(budget_list):
+        budget_sum += min(val, mid)
+
+    if start == end - 1:
+        return binary_search(end, end)
+
+    if budget_sum > total_budget:
+        if start == end:
+            return mid - 1
+
+        return binary_search(start, mid)
+    elif budget_sum < total_budget:
+        if start == end:
+            return mid
+
+        return binary_search(mid, end)
+    else:
+        return mid
+
+
+if __name__ == '__main__':
+    n = int(input())
+    budget_list = list(map(int, input().split()))
+    total_budget = int(input())
+    sol = solution()
+    print(sol)
