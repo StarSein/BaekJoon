f31e0b047c5c2d7a681bdc77d2d58a7453ac00c0
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-21 19:23:58 +0900

    18222.2.1
    AC

--- boj18222.java
+++ boj18222.java
@@ -4,21 +4,24 @@ public class boj18222 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             long k = scanner.nextLong();
-
-            int cntSwap = 0;
-            while (k > 1) {
-                long t = 1;
-                while (2 * t < k) {
-                    t *= 2;
-                }
-                k -= t;
-                cntSwap += 1;
-            }
-            if (cntSwap % 2 == 0) {
-                System.out.print('0');
-            } else {
-                System.out.print('1');
-            }
+            char sol = divide_conquer(k);
+            System.out.print(sol);
         }
+    }
+
+    static double baseLog(double x, double base) {
+        return Math.log10(x) / Math.log10(base);
+    }
+
+    static char divide_conquer(long k) {
+        if (k == 1) {
+            return '0';
+        }
+
+        int expr = (int) baseLog(k-1, 2);
+        long biggest_power_of_two = (long) Math.pow(2, expr);
+        char swapped_from = divide_conquer(k - biggest_power_of_two);
+        if (swapped_from == '0') return '1';
+        else                     return '0'; 
     }
 }
