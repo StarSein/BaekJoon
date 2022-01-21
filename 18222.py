e796a75568d94b96671b4b6ca4cd0b54fe7d2ed0
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-21 13:30:17 +0900

    18222.2.1
    AC
    
    이전 풀이가 규칙성으로부터 값을 도출한 것이라면,
    이 풀이는 이전 풀이의 배경이 되는 알고리즘을 구현한 것이다.

--- 18222.py
+++ 18222.py
@@ -2,19 +2,23 @@ import sys
 from math import log2
 
 input = sys.stdin.readline
+INIT_CHAR = '0'
+
+swap = {'0': '1',
+        '1': '0'}
 
 
 def solution(k: int) -> chr:
-    cnt_swap = 0
-    while k > 1:
-        biggest_power_of_2 = 2 ** int(log2(k-1))
-        k -= biggest_power_of_2
-        cnt_swap += 1
-    if cnt_swap % 2 == 0:
-        char = '0'
-    else:
-        char = '1'
+    char = divide_conquer(k)
     return char
+
+
+def divide_conquer(k: int) -> chr:
+    if k == 1:
+        return INIT_CHAR
+
+    swapped_from = divide_conquer(k - 2**int(log2(k-1)))
+    return swap.get(swapped_from)
 
 
 if __name__ == '__main__':
