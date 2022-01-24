86f70ab74974890bd3365696623e4a08cf1103ec
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-24 23:55:55 +0900

    5427.1.2
    AC !!!

--- 5427.py
+++ 5427.py
@@ -12,9 +12,12 @@ ROW = 1
 
 
 def solution() -> str:
+    time = 1
     for col in range(h):
         for row in range(w):
             if building[col][row] == '@':
+                if is_escape_spot(col, row):
+                    return str(time)
                 INIT_SITE = (col, row)
                 break
     man_sites = [INIT_SITE]
@@ -24,8 +27,6 @@ def solution() -> str:
             if building[col][row] == '*':
                 edge_fire_sites.append((col, row))
 
-
-    time = 1
     is_escaped = False
     while not is_escaped:
         if len(man_sites) == 0:
@@ -37,7 +38,7 @@ def solution() -> str:
                 next_col = fire[COL] + dy[dir]
                 next_row = fire[ROW] + dx[dir]
                 if 0 <= next_col < h and 0 <= next_row < w:
-                    if building[next_col][next_row] == '.' or building[next_col][next_row] == '@': # '@'이 있던 곳에 불길이 퍼지는 것은 계산에 무의미함
+                    if building[next_col][next_row] == '.' or building[next_col][next_row] == '@':
                         next_efs.append((next_col, next_row))
                         building[next_col][next_row] = '*'
 
@@ -48,7 +49,7 @@ def solution() -> str:
                 next_row = man[ROW] + dx[dir]
                 if 0 <= next_col < h and 0 <= next_row < w:
                     if building[next_col][next_row] == '.':
-                        if next_col == 0 or next_col == h-1 or next_row == 0 or next_row == w-1:
+                        if is_escape_spot(next_col, next_row):
                             is_escaped = True
                         else:
                             next_ms.append((next_col, next_row))
@@ -58,6 +59,13 @@ def solution() -> str:
         man_sites = next_ms[:]
         time += 1
     return str(time)
+
+
+def is_escape_spot(col: int, row: int) -> bool:
+    if col == 0 or col == h - 1 or row == 0 or row == w - 1:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
