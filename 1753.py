54bc3e7b2949ffebe378378302dda321df98714e
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-26 11:48:05 +0900

    1753.1.1
    WA
    
    다익스트라 알고리즘을 이용한 풀이.
    "서로 다른 두 정점 사이에 여러 개의 간선이 존재할 수도 있음에 유의한다."라는 지문을 놓쳤다.

--- 1753.py
+++ 1753.py
@@ -1,25 +1,38 @@
 import sys
+from collections import deque
 
 
-def floyd_warshall():
-    for k in range(1, V + 1):
-        for r in range(1, V + 1):
-            for s in range(1, V + 1):
-                graph[r][s] = min(graph[r][s], graph[r][k] + graph[k][s])
+input = sys.stdin.readline
+INF = 3_000_000
+ARRIVE = 0
+WEIGHT = 1
 
 
-INF = sys.maxsize
-V, E = map(int, sys.stdin.readline().split())
-K = int(sys.stdin.readline())
-graph = [[INF for i in range(V + 1)] for j in range(V + 1)]
-for _ in range(E):
-    u, v, w = map(int, sys.stdin.readline().split())
-    graph[u][v] = w
-floyd_warshall()
-for row in range(1, V + 1):
-    if K == row:
-        print(0)
-    elif graph[K][row] == INF:
-        print("INF")
-    else:
-        print(graph[K][row])
+def solution():
+    dists = [INF] * (num_v + 1)
+    dists[start_point] = 0
+    visit_queue = deque([(start_point, city, weight) for city, weight in next_roads[start_point]])   # (출발 도시, 도착 도시, 가중치)를 담는 배열
+    while len(visit_queue):
+        depart, arrive, weight = visit_queue.popleft()
+        dists[arrive] = min(dists[arrive], dists[depart] + weight)
+        cnt_not_visited[arrive] -= 1
+        if cnt_not_visited[arrive] == 0:
+            for idx, road in enumerate(next_roads[arrive]):
+                visit_queue.append((arrive, road[ARRIVE], road[WEIGHT]))
+    for i in range(1, len(dists)):
+        if dists[i] == INF:
+            print("INF")
+        else:
+            print(f"{dists[i]}")
+
+
+if __name__ == '__main__':
+    num_v, num_e = map(int, input().split())
+    start_point = int(input())
+    next_roads = [[] for city in range(num_v + 1)]         # 출발도시(idx)에 대해 (도착도시, 가중치)의 리스트를 담는 리스트
+    cnt_not_visited = [0] * (num_v + 1)     # 해당 도시로 향하는 도로 중 방문하지 않은 도로의 개수를 담는 리스트
+    for i in range(num_e):
+        u, v, e = map(int, input().split())
+        next_roads[u].append((v, e))
+        cnt_not_visited[v] += 1
+    solution()
