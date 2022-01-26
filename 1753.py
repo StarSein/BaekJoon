bb0a54c35c1da0c992b1de806b444e2bf910dbb0
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-26 12:55:47 +0900

    1753.2.1
    WA
    
    이전 풀이의 경우, A->B와 B->A인 도로가 하나라도 있으면 logic error가 발생한다.
    게다가 다익스트라가 아닌 SPFA에 가까웠다.
    이 풀이는 다익스트라 알고리즘을 구현해본 것이다.
    왜 틀렸는지는 아직 모르겠다.

--- 1753.py
+++ 1753.py
@@ -1,5 +1,5 @@
 import sys
-from collections import deque
+import heapq
 
 
 input = sys.stdin.readline
@@ -10,17 +10,21 @@ WEIGHT = 1
 
 def solution():
     dists = [INF] * (num_v + 1)
-    dists[start_point] = 0
-    visit_queue = deque([(start_point, city, weight) for city, weight in next_roads[start_point]])   # (출발 도시, 도착 도시, 가중치)를 담는 배열
-    while len(visit_queue):
-        depart, arrive, weight = visit_queue.popleft()
-        dists[arrive] = min(dists[arrive], dists[depart] + weight)
-        cnt_not_visited[arrive] -= 1
-        if cnt_not_visited[arrive] == 0:
-            for idx, road in enumerate(next_roads[arrive]):
-                visit_queue.append((arrive, road[ARRIVE], road[WEIGHT]))
+    is_visited = [False] * (num_v + 1)
+    dists[k] = 0
+    is_visited[k] = True
+    heap = [(weight, k, city) for city, weight in next_roads[k]]  # (가중치, 출발 도시, 도착 도시)를 담는 힙
+    heapq.heapify(heap)
+    while len(heap):
+        weight, depart, current = heapq.heappop(heap)
+        dists[current] = min(dists[current], dists[depart] + weight)
+        if not is_visited[current]:
+            for idx, road in enumerate(next_roads[current]):
+                heapq.heappush(heap, (road[WEIGHT], current, road[ARRIVE]))
+            is_visited[current] = True
+
     for i in range(1, len(dists)):
-        if dists[i] >= INF:
+        if dists[i] == INF:
             print("INF")
         else:
             print(f"{dists[i]}")
@@ -28,11 +32,9 @@ def solution():
 
 if __name__ == '__main__':
     num_v, num_e = map(int, input().split())
-    start_point = int(input())
+    k = int(input())
     next_roads = [[] for city in range(num_v + 1)]         # 출발도시(idx)에 대해 (도착도시, 가중치)의 리스트를 담는 리스트
-    cnt_not_visited = [0] * (num_v + 1)     # 해당 도시로 향하는 도로 중 방문하지 않은 도로의 개수를 담는 리스트
     for i in range(num_e):
         u, v, e = map(int, input().split())
         next_roads[u].append((v, e))
-        cnt_not_visited[v] += 1
     solution()
