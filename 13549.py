eb350444e3ce8991a282a1f976709b454802c2e5
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-26 18:43:27 +0900

    13549.1.1
    WA

--- 13549.py
+++ 13549.py
@@ -1,25 +1,40 @@
 import sys
+import heapq
 
 
-N, K = map(int, sys.stdin.readline().split())
-site_s = set()
-if N >= K:
-    time = N - K
-else:
-    i = N
-    site_s.add(i * 2)
-    time = 0
-    while True:
-        if K in site_s:
-            break
-        temp = set()
-        for e in site_s:
-            temp.add(e + 1)
-            temp.add(e - 1)
-        set_add = set()
-        for el in temp:
-            if el < K:
-                set_add.add(el * 2)
-        site_s = temp.union(set_add)
-        time += 1
-print(time)
+input = sys.stdin.readline
+MAX_X = 100_000
+INF = MAX_X
+WALK_COST = 1
+WARP_COST = 0
+
+
+def solution() -> int:
+    dists = [INF] * (MAX_X + 1)
+    heap = [(0, n)]
+    while len(heap):
+        dist, x = heapq.heappop(heap)
+
+        if x == k:
+            return dist
+
+        if 0 <= x-1 and dists[x-1] == INF:
+            dists[x-1] = dist + WALK_COST
+            heapq.heappush(heap, (dists[x-1], x - 1))
+        if x+1 <= MAX_X and dists[x+1] == INF:
+            dists[x+1] = dist + WALK_COST
+            heapq.heappush(heap, (dists[x+1], x + 1))
+        if x == 0:
+            continue
+
+        i = 2 * x
+        while i <= MAX_X and dists[i] == INF:
+            dists[i] = dist + WARP_COST
+            heapq.heappush(heap, (dists[i], i))
+            i *= 2
+
+
+if __name__ == '__main__':
+    n, k = map(int, input().split())
+    sol = solution()
+    print(sol)
