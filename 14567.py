7bfff7ad4bd7e5db89575b1be64b1a1123c777a2
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-27 11:42:59 +0900

    14567.1.2
    AC
    
    코드의 길이를 줄였다.

--- 14567.py
+++ 14567.py
@@ -1,38 +1,34 @@
 import sys
+from typing import List
 
 
 input = sys.stdin.readline
 
 
-def solution(n, post_list, pre_list):
-    res_list = [0] * (n+1)
+def solution() -> List[str]:
+    res = ['0'] * (n + 1)
     semester = 1
-    checked_subj_cnt = 0
-    while checked_subj_cnt < n:
-        subtracting_pre_list = []
-        for subj in range(1, n+1):
-            if pre_list[subj] == 0:
-                res_list[subj] = semester
-                pre_list[subj] -= 1
-                checked_subj_cnt += 1
-                for post in post_list[subj]:
-                    subtracting_pre_list.append(post)
-        for subj in subtracting_pre_list:
-            pre_list[subj] -= 1
-        semester += 1
+    while True:                                                 # study_list: 해당 학기에 이수할 과목(아직 배우지 않았으면서, 아직 듣지 않은 선수과목의 수가 0인 과목)
+        study_list = [subject for subject in range(1, n + 1) if res[subject] == '0' and precede_cnts[subject] == 0]
+        if len(study_list) == 0:                                # 해당 학기에 들을 과목이 없으면 반복문을 중단한다
+            break
 
-    return res_list
+        for idx, subject in enumerate(study_list):              # 해당 학기에 듣는 과목에 대해 지금이 몇 번째 학기인지를 입력한다
+            res[subject] = str(semester)
 
-                    
+            for i, follow in enumerate(followers[subject]):     # 해당 학기에 듣는 과목의 후행과목에 대해, '아직 듣지 않은 선수과목의 수'를 1만큼 차감한다
+                precede_cnts[follow] -= 1
+        semester += 1                                           # 다음 학기로 넘어간다
+    return res[1:]
+
+
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    post_list = [[] for _ in range(n+1)]
-    pre_list = [0] * (n+1)
+    precede_cnts = [0] * (n + 1)                # precede_cnts: idx 번 과목의 선수과목의 개수를 담은 리스트
+    followers = [[] for _ in range(n + 1)]      # followers: idx 번 과목이 선수과목인 과목들의 목록을 담은 리스트
     for i in range(m):
-        pre, post = map(int, input().split())
-        post_list[pre].append(post)
-        pre_list[post] += 1
-    res_list = solution(n, post_list, pre_list)
-
-    for subj in range(1, n+1):
-        print(res_list[subj], end=' ')
+        a, b = map(int, input().split())
+        followers[a].append(b)
+        precede_cnts[b] += 1
+    sol = solution()
+    print(' '.join(sol))
