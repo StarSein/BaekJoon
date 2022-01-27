7e0bad0537c5fe386c44a7fc0d70ebc4532e02bd
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-01-27 19:02:46 +0900

    20210.1.2
    AC
    
    '<' 연산이나 '>' 연산 둘 중 하나만 정의되어 있으면 sort 메소드가 작동할 수 있다.
    '==' 연산은 사용되지 않는다.

--- 20210.py
+++ 20210.py
@@ -43,54 +43,6 @@ class Mystring:
                     continue
         return len(self.arr) < len(other.arr)
 
-    def __gt__(self, other): # '>'
-        length = min(len(self.arr), len(other.arr))
-        for i in range(length):
-            if self.arr[i].isdigit() and other.arr[i].isdigit():
-                if int(self.arr[i]) == int(other.arr[i]):
-                    if len(self.arr[i]) != len(other.arr[i]):
-                        return len(self.arr[i]) > len(other.arr[i])
-                    else:
-                        continue
-                else:
-                    return int(self.arr[i]) > int(other.arr[i])
-
-            elif self.arr[i].isdigit() or other.arr[i].isdigit():
-                return other.arr[i].isdigit()
-
-            else:
-                tmpA = self.arr[i].lower()
-                tmpB = other.arr[i].lower()
-                if tmpA != tmpB:
-                    return tmpA > tmpB
-                elif self.arr[i] != other.arr[i]:
-                    return self.arr[i].islower()
-                else:
-                    continue
-        return len(self.arr) > len(other.arr)
-
-    def __eq__(self, other): # '=='
-        length = min(len(self.arr), len(other.arr))
-        for i in range(length):
-            if self.arr[i].isdigit() and other.arr[i].isdigit():
-                if int(self.arr[i]) == int(other.arr[i]):
-                    if len(self.arr[i]) != len(other.arr[i]):
-                        return False
-                    else:
-                        continue
-                else:
-                    return False
-
-            elif self.arr[i].isdigit() or other.arr[i].isdigit():
-                return False
-
-            else:
-                if self.arr[i] != other.arr[i]:
-                    return False
-                else:
-                    continue
-        return len(self.arr) == len(other.arr)
-
     def __str__(self):
         return self.s
 
