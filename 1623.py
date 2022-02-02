2588fb0ce701cf5984af732f39e7cb017e2c91c8
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-02 15:49:50 +0900

    1623.1.2
    AC !!!
    
    168488KB / 128MB
    3312ms / 2s
    
    메모리 소모를 줄이기 위해, 재귀 호출이 없도록 했다.
    이전 풀이에서 후위 순회를 하던 make_dp는 스택을 이용한 층별 순회로 구현했고,
    이전 풀이에서 전위 순회를 하던 find_optimal_lineup은 큐를 이용한 층별 순회로 구현했다.

--- 1623.py
+++ 1623.py
@@ -1,7 +1,6 @@
 import sys
+from collections import deque
 
-
-sys.setrecursionlimit(250_000)
 
 input = sys.stdin.readline
 root_node = 1
@@ -17,17 +16,14 @@ def solution():
     make_dp(root_node)
     print(dp[root_node][True][OPTIMAL_SCORE], dp[root_node][False][OPTIMAL_SCORE])
 
-    global lineup
-    lineup = []
-    find_optimal_lineup(root_node, True)
+    lineup = find_optimal_lineup(root_node, True)
     lineup.sort()
     lineup.append(-1)
     for idx, val in enumerate(lineup):
         print(val, end=' ')
     print()
 
-    lineup = []
-    find_optimal_lineup(root_node, False)
+    lineup = find_optimal_lineup(root_node, False)
     lineup.sort()
     lineup.append(-1)
     for idx, val in enumerate(lineup):
@@ -35,30 +31,45 @@ def solution():
     print()
 
 
-def make_dp(node: int):
-    num_child = len(childs[node])
-    dp[node][False] = [DEFAULT_SCORE, [False] * num_child]
-    dp[node][True] = [scores[node-1], [False] * num_child]
-    for i in range(num_child):
-        child = childs[node][i]
-        make_dp(child)
+def make_dp(start_node: int):
+    st_make = []
+    st_visit = [start_node]
+    while len(st_make) < n:
+        next_visit = []
+        while len(st_visit):
+            current_node = st_visit.pop()
+            for i in range(len(childs[current_node])):
+                next_visit.append(childs[current_node][i])
+            st_make.append(current_node)
+        st_visit = next_visit[:]
+        
+    while len(st_make):
+        node = st_make.pop()
+        num_child = len(childs[node])
+        dp[node][False] = [DEFAULT_SCORE, [False] * num_child]
+        dp[node][True] = [scores[node-1], [False] * num_child]
+        for i in range(num_child):
+            child = childs[node][i]
 
-        is_child_in = (dp[child][True][OPTIMAL_SCORE] > dp[child][False][OPTIMAL_SCORE])
-        dp[node][False][OPTIMAL_SCORE] += dp[child][is_child_in][OPTIMAL_SCORE]
-        dp[node][False][IS_CHILD_IN][i] = is_child_in
+            is_child_in = (dp[child][True][OPTIMAL_SCORE] > dp[child][False][OPTIMAL_SCORE])
+            dp[node][False][OPTIMAL_SCORE] += dp[child][is_child_in][OPTIMAL_SCORE]
+            dp[node][False][IS_CHILD_IN][i] = is_child_in
 
-        dp[node][True][OPTIMAL_SCORE] += dp[child][False][OPTIMAL_SCORE]
+            dp[node][True][OPTIMAL_SCORE] += dp[child][False][OPTIMAL_SCORE]
 
 
-def find_optimal_lineup(current_node: int, is_current_in: bool):
-    global lineup
+def find_optimal_lineup(start_node: int, is_start_in: bool):
+    lineup = []
+    q_visit = deque([(start_node, is_start_in)])
+    while len(q_visit):
+        current_node, is_current_in = q_visit.popleft()
+        if is_current_in:
+            lineup.append(current_node)
+        for i in range(len(childs[current_node])):
+            is_child_in = dp[current_node][is_current_in][IS_CHILD_IN][i]
+            q_visit.append((childs[current_node][i], is_child_in))
 
-    if is_current_in:
-        lineup.append(current_node)
-
-    for idx, child in enumerate(childs[current_node]):
-        is_child_in = dp[current_node][is_current_in][IS_CHILD_IN][idx]
-        find_optimal_lineup(child, is_child_in)
+    return lineup
 
 
 if __name__ == '__main__':
@@ -68,5 +79,5 @@ if __name__ == '__main__':
     childs = [[] for node in range(n + 1)]
     for idx, val in enumerate(parents, start=2):
         childs[val].append(idx)
-    dp = [[0, []] for col in range(n + 1)]
+    dp = [[[], []] for col in range(n + 1)]
     solution()
