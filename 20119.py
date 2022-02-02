7e3efe3cca45d9048598645e0709393c9b0600bd
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-02 20:11:55 +0900

    20119.1.2
    AC
    
    하나의 포션을 만드는 레시피의 수가 1개라는 보장이 없었다.
    이를 반영하여 탐색 및 삭제 작업의 시간 복잡도가 O(1)인 집합을 사용해 풀었다.

--- 20119.py
+++ 20119.py
@@ -6,35 +6,43 @@ input = sys.stdin.readline
 
 
 def solution():
-    for idx, potion in enumerate(y_list):
-        cnt_precede[potion] = 0
     q_possess = deque(y_list)
-    list_potion = []
+    is_possess = [False] * n
     while len(q_possess):
         potion = q_possess.popleft()
-        list_potion.append(potion)
+        is_possess[potion-1] = True
         for idx, follower in enumerate(followers[potion]):
-            cnt_precede[follower] -= 1
-            if cnt_precede[follower] == 0:
-                q_possess.append(follower)
+            if is_possess[follower-1]:
+                continue
 
-    print(len(list_potion))
-    list_potion.sort()
-    for idx, potion in enumerate(list_potion):
-        print(potion, end=' ')
+            for i in range(len(preceders[follower])):
+                if potion in preceders[follower][i]:
+                    preceders[follower][i].discard(potion)
+                    if len(preceders[follower][i]) == 0:
+                        q_possess.append(follower)
+
+    print(sum(is_possess))
+    for potion, is_able in enumerate(is_possess, start=1):
+        if is_able:
+            print(potion, end=' ')
 
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    cnt_precede = [0] * (n + 1)
-    followers = [[] for node in range(n + 1)]
+    preceders = [[set()] for node in range(n + 1)]
+    followers = [set() for node in range(n + 1)]
+    is_recipe = [False] * (n + 1)
     for inp in range(m):
         recipe = list(map(int, input().split()))
         k = recipe[0]
         r = recipe[-1]
-        cnt_precede[r] = k
+        if is_recipe[r]:
+            preceders[r].append(set())
+        else:
+            is_recipe[r] = True
         for i in range(1, k + 1):
-            followers[recipe[i]].append(r)
+            followers[recipe[i]].add(r)
+            preceders[r][-1].add(recipe[i])
     l = int(input())
     y_list = list(map(int, input().split()))
     solution()
