0cfdbf5d2e6b8a73635b3c853da5d469aeeea4df
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-02 18:31:48 +0900

    15681.1.2
    AC

--- 15681.py
+++ 15681.py
@@ -1,41 +1,34 @@
 import sys
 from collections import deque
 
-sys.setrecursionlimit(6)
-
+sys.setrecursionlimit(100_003)
 input = sys.stdin.readline
+HEAD = 0
 
 
 def solution():
-    make_tree(r, -1, 2)
-    cnt_nodes_of_subtree(r)
+    make_tree(r, HEAD)
+    
     while len(q_query):
         query = q_query.popleft()
-        print(cnt_nos[query])
+        print(dp[query])
 
 
-def make_tree(current: int, parent: int, depth: int):
-    for idx, adjacent in enumerate(connected[current]):
+def make_tree(current: int, parent: int) -> int:
+    for idx, adjacent in enumerate(adjacents[current]):
         if adjacent != parent:
-            childs[current].append(adjacent)
-            make_tree(adjacent, current, depth + 1)
-
-
-def cnt_nodes_of_subtree(current: int):
-    cnt_nos[current] = 1
-    for idx, child in enumerate(childs[current]):
-        cnt_nos[current] += cnt_nodes_of_subtree(child)
-    return cnt_nos[current]
+            child = adjacent
+            dp[current] += make_tree(child, current)
+    return dp[current]
 
 
 if __name__ == '__main__':
     n, r, q = map(int, input().split())
-    connected = [[] for node in range(n+1)]
+    adjacents = [[] for node in range(n+1)]
     for edge in range(n-1):
         u, v = map(int, input().split())
-        connected[u].append(v)
-        connected[v].append(u)
+        adjacents[u].append(v)
+        adjacents[v].append(u)
     q_query = deque([int(input()) for query in range(q)])
-    childs = [[] for node in range(n+1)]
-    cnt_nos = [0] * (n+1)
+    dp = [1] * (n + 1)
     solution()
