81e75fed16804360b05afb855a80dbc7ed881509
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-02 16:24:57 +0900

    1623.2.1
    AC !!!
    
    111212KB / 128MB
    1992ms / 2s
    
    이전 풀이에서는 dp[node][IS_NODE_IN]의 두 번째 인덱스에 해당 케이스가 포함하는 '자식 노드들의 리스트'도 저장했기 때문에, 메모리와 시간 소모가 컸다.
    이 풀이에서는 find_opt_lineup 함수에서 트리를 층별 순회를 하면서 q_visit에 최적이 되는 (노드, 노드 포함 여부)를 저장하여 참조하는 방식으로 구현했다.
    현재 노드의 번호와 파티 참석 여부를 알고 있다면, 자식 노드의 참석 여부 또한 dp를 바탕으로 구할 수 있으므로, 굳이 이전 풀이처럼 저장해놓을 필요가 없었다.

--- 1623.py
+++ 1623.py
@@ -1,74 +1,69 @@
 import sys
 from collections import deque
+from copy import deepcopy
+from typing import List
 
 
 input = sys.stdin.readline
-root_node = 1
-DEFAULT_SCORE = 0
-END_OF_TREE = 0
-
-# indices in dp[node][is_node_in][]
-OPTIMAL_SCORE = 0
-IS_CHILD_IN = 1
+ROOT = 1
+INIT_SCORE = 0
 
 
 def solution():
-    make_dp(root_node)
-    print(dp[root_node][True][OPTIMAL_SCORE], dp[root_node][False][OPTIMAL_SCORE])
+    make_dp(ROOT)
+    print(dp[ROOT][1], dp[ROOT][0])
 
-    lineup = find_optimal_lineup(root_node, True)
-    lineup.sort()
-    lineup.append(-1)
+    lineup = find_opt_lineup(ROOT, True)
     for idx, val in enumerate(lineup):
         print(val, end=' ')
     print()
 
-    lineup = find_optimal_lineup(root_node, False)
-    lineup.sort()
-    lineup.append(-1)
+    lineup = find_opt_lineup(ROOT, False)
     for idx, val in enumerate(lineup):
         print(val, end=' ')
     print()
 
 
-def make_dp(start_node: int):
+def make_dp(start: int):
     st_make = []
-    st_visit = [start_node]
+    st_visit = [start]
     while len(st_make) < n:
         next_visit = []
         while len(st_visit):
-            current_node = st_visit.pop()
-            for i in range(len(childs[current_node])):
-                next_visit.append(childs[current_node][i])
-            st_make.append(current_node)
-        st_visit = next_visit[:]
-        
+            current = st_visit.pop()
+            st_make.append(current)
+            for i in range(len(childs[current])):
+                next_visit.append(childs[current][i])
+        st_visit = deepcopy(next_visit)
+
     while len(st_make):
         node = st_make.pop()
-        num_child = len(childs[node])
-        dp[node][False] = [DEFAULT_SCORE, [False] * num_child]
-        dp[node][True] = [scores[node-1], [False] * num_child]
-        for i in range(num_child):
+        dp[node][0] = INIT_SCORE
+        dp[node][1] = scores[node-1]
+        for i in range(len(childs[node])):
             child = childs[node][i]
-
-            is_child_in = (dp[child][True][OPTIMAL_SCORE] > dp[child][False][OPTIMAL_SCORE])
-            dp[node][False][OPTIMAL_SCORE] += dp[child][is_child_in][OPTIMAL_SCORE]
-            dp[node][False][IS_CHILD_IN][i] = is_child_in
-
-            dp[node][True][OPTIMAL_SCORE] += dp[child][False][OPTIMAL_SCORE]
+            dp[node][0] += max(dp[child][0], dp[child][1])
+            dp[node][1] += dp[child][0]
 
 
-def find_optimal_lineup(start_node: int, is_start_in: bool):
+def find_opt_lineup(start: int, is_start_in: bool) -> List[int]:
     lineup = []
-    q_visit = deque([(start_node, is_start_in)])
+    q_visit = deque([(start, is_start_in)])
     while len(q_visit):
-        current_node, is_current_in = q_visit.popleft()
+        current, is_current_in = q_visit.popleft()
         if is_current_in:
-            lineup.append(current_node)
-        for i in range(len(childs[current_node])):
-            is_child_in = dp[current_node][is_current_in][IS_CHILD_IN][i]
-            q_visit.append((childs[current_node][i], is_child_in))
-
+            lineup.append(current)
+            for i in range(len(childs[current])):
+                child = childs[current][i]
+                is_child_in = False
+                q_visit.append((child, is_child_in))
+        else:
+            for i in range(len(childs[current])):
+                child = childs[current][i]
+                is_child_in = (dp[child][1] > dp[child][0])
+                q_visit.append((child, is_child_in))
+    lineup.sort()
+    lineup.append(-1)
     return lineup
 
 
@@ -79,5 +74,5 @@ if __name__ == '__main__':
     childs = [[] for node in range(n + 1)]
     for idx, val in enumerate(parents, start=2):
         childs[val].append(idx)
-    dp = [[[], []] for col in range(n + 1)]
+    dp = [[0, 0] for node in range(n + 1)]
     solution()
