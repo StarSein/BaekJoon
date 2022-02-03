cb1e3f8c517894135b0c28b0623ecaa1e37c468b
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-03 19:04:01 +0900

    1197.1.2
    AC
    
    사이클을 만들지 않기 위해서는 분리 집합을 활용한 union & find 함수를 사용해야 한다.
    이전 풀이의 경우에는 두 connection을 연결하는 하나의 간선도 사이클로 간주하는 오류가 있다.

--- 1197.py
+++ 1197.py
@@ -5,17 +5,31 @@ input = sys.stdin.readline
 
 
 def solution():
-    is_checked = [False] * (v + 1)
     res = 0
     while len(min_heap):
         weight, node_a, node_b = heapq.heappop(min_heap)
-        if is_checked[node_a] and is_checked[node_b]:
+        if find_root(node_a) == find_root(node_b):
             continue
         res += weight
-        is_checked[node_a] = True
-        is_checked[node_b] = True
+        union(node_a, node_b)
 
     print(res)
+
+
+def union(a: int, b: int):
+    a_root = find_root(a)
+    b_root = find_root(b)
+
+    if a_root != b_root:
+        roots[b_root] = a_root
+
+
+def find_root(x: int) -> int:
+    if roots[x] == x:
+        return x
+
+    roots[x] = find_root(roots[x])
+    return roots[x]
 
 
 if __name__ == '__main__':
@@ -24,4 +38,5 @@ if __name__ == '__main__':
     for edge in range(e):
         a, b, c = map(int, input().split())
         heapq.heappush(min_heap, (c, a, b))
+    roots = [node for node in range(v + 1)]
     solution()
