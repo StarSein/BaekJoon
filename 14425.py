e47ab258bd93e5d18ad825420e6a9e0b6b7af079
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-04 21:27:49 +0900

    14425.2.1
    AC
    
    이전 풀이는 트라이 알고리즘을 사용한 것으로, PyPy3으로 채점했을 때 메모리 및 시간 제한을 약 10% 넘으면서 AC를 받았다.
    이 풀이는 해쉬 충돌만 없다면 항상 O(1)의 시간 복잡도로 원소 추가 및 탐색 작업을 해내는 set 컨테이너를 활용한 것이다.

--- 14425.py
+++ 14425.py
@@ -1,53 +1,20 @@
 import sys
-from collections import defaultdict
 
 
 input = sys.stdin.readline
-NUM_CHAR = 26
-ASCII_a = 97
-
-
-class Trie:
-    def __init__(self):
-        self.is_terminal = False
-        self.children = defaultdict(int)
-
-    def insert(self, i):
-        if i == len(inp_str):
-            self.is_terminal = True
-            return
-
-        index = ord(inp_str[i]) - ASCII_a
-        if self.children[index] == 0:
-            self.children[index] = Trie()
-
-        self.children[index].insert(i + 1)
-
-    def is_exist(self, i) -> bool:
-        if i == len(inp_str):
-            if self.is_terminal:
-                return True
-            else:
-                return False
-
-        index = ord(inp_str[i]) - ASCII_a
-        if self.children[index] == 0:
-            return False
-
-        return self.children[index].is_exist(i + 1)
 
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
-
-    root = Trie()
+    str_set = set()
     for insert_str in range(n):
         inp_str = input().rstrip()
-        root.insert(0)
+        str_set.add(inp_str)
 
     cnt = 0
     for check_str in range(m):
         inp_str = input().rstrip()
-        if root.is_exist(0):
+        if inp_str in str_set:
             cnt += 1
+
     print(cnt)
