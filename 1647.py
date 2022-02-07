9257b5af061775a0f8e89b00ee90bbadb7b25ce3
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-07 17:55:50 +0900

    1647.2.1
    AC in Python 3
    
    크루스칼 알고리즘을 이용한 풀이

--- 1647.py
+++ 1647.py
@@ -3,32 +3,43 @@ import heapq
 
 
 input = sys.stdin.readline
-START = 1
 
 
 def solution():
-    is_visited = [False] * (n + 1)
-    min_heap = [(0, START)]
+    heapq.heapify(min_heap)
     mst_list = []
     while len(min_heap):
-        cost, arrive = heapq.heappop(min_heap)
-        if is_visited[arrive]:
+        cost, node_a, node_b = heapq.heappop(min_heap)
+
+        a_root = find_root(node_a)
+        b_root = find_root(node_b)
+        if a_root == b_root:
             continue
 
+        union(a_root, b_root)
         mst_list.append(cost)
-        is_visited[arrive] = True
-        for idx, edge in enumerate(connected[arrive]):
-            heapq.heappush(min_heap, (edge[0], edge[1]))
 
     res = sum(mst_list) - max(mst_list)
     print(res)
 
 
+def find_root(x: int) -> int:
+    if roots[x] == x:
+        return x
+
+    roots[x] = find_root(roots[x])
+    return roots[x]
+
+
+def union(a_root: int, b_root: int):
+    roots[b_root] = a_root
+
+
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    connected = [[] for node in range(n + 1)]
+    roots = [num for num in range(n + 1)]
+    min_heap = []
     for edge in range(m):
         a, b, c = map(int, input().split())
-        connected[a].append((c, b))
-        connected[b].append((c, a))
+        heapq.heappush(min_heap, (c, a, b))
     solution()
