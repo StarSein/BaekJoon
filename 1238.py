fce3696cbce57582b7bbb70b04230b08c703973b
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-08 19:14:36 +0900

    1238.1.2
    AC
    
    이전 풀이보다 배열을 한 개 더 많이 사용하지만, 가독성은 개선한 풀이

--- 1238.py
+++ 1238.py
@@ -3,34 +3,45 @@ import heapq
 
 
 input = sys.stdin.readline
+MIN_COST = 0
 
 
-def dijkstra_go(start: int) -> int:
+def solution() -> int:
+    dijkstra_come(x)                                # x에서 각 노드까지의 최단 거리를 구하는 함수
+    come_costs.pop(0)
+    best_cost = MIN_COST
+    for start_node, come_cost in enumerate(come_costs, start=1):
+        go_cost = dijkstra_go(start_node, x)        # start_node에서 x까지의 최단 거리를 구하는 함수
+        current_cost = go_cost + come_cost
+        best_cost = max(best_cost, current_cost)
+
+    return best_cost
+
+
+def dijkstra_go(start: int, end: int) -> int:
     min_heap = [(0, start)]
-    is_visit = [False] * (n + 1)
+    is_visited = [False] * (n + 1)
     while len(min_heap):
         cost, current = heapq.heappop(min_heap)
-        if is_visit[current]:
+        if is_visited[current]:
             continue
 
-        if current == x:
+        if current == end:
             return cost
 
-        is_visit[current] = True
+        is_visited[current] = True
         for idx, val in enumerate(connected[current]):
             heapq.heappush(min_heap, (cost + val[0], val[1]))
 
 
-def dijkstra_come():
-    min_heap = [(0, x)]
-    is_visit = [False] * (n + 1)
+def dijkstra_come(start: int):
+    min_heap = [(0, start)]
     while len(min_heap):
         cost, current = heapq.heappop(min_heap)
-        if is_visit[current]:
+        if come_costs[current] != -1:
             continue
 
-        times[current] += cost
-        is_visit[current] = True
+        come_costs[current] = cost
 
         for idx, val in enumerate(connected[current]):
             heapq.heappush(min_heap, (cost + val[0], val[1]))
@@ -40,11 +51,8 @@ if __name__ == '__main__':
     n, m, x = map(int, input().split())
     connected = [[] for node in range(n + 1)]
     for edge in range(m):
-        depart, arrive, cost = map(int, input().split())
-        connected[depart].append((cost, arrive))
-
-    times = [0] * (n + 1)
-    for node in range(1, n + 1):
-        times[node] = dijkstra_go(node)
-    dijkstra_come()
-    print(max(times))
+        depart, arrive, t = map(int, input().split())
+        connected[depart].append((t, arrive))
+    come_costs = [-1] * (n + 1)
+    sol = solution()
+    print(sol)
