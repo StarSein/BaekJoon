fcb70cacddd1e1e581b2b2470f894b1a44e4fc74
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-09 20:38:39 +0900

    1238.3.1
    AC
    
    입력받은 단방향 간선들의 방향을 바꾼 역방향 그래프를 따로 저장하면
    단 두 번의 다익스트라 알고리즘 이용으로도 문제를 풀 수 있다.
    각 노드에서 x까지 단방향 그래프를 통한 최단 거리 == x에서 각 노드까지 역방향 그래프를 통한 최단 거리

--- 1238.py
+++ 1238.py
@@ -1,65 +1,29 @@
 import sys
 import heapq
-from collections import deque
+from typing import List
 
 
 input = sys.stdin.readline
-DEFAULT = -1
 
 
-def solution() -> int:
-    dijkstra_come(x)
-    q_calc = [(-come_costs[node], node) for node in range(1, n + 1)]
-    heapq.heapify(q_calc)
-    while len(q_calc):
-        start_node = heapq.heappop(q_calc)[1]
-        if go_costs[start_node] != DEFAULT:
-            continue
-        dijkstra_go(start_node, x)
-
-    best_cost = 0
-    for node in range(1, n + 1):
-        current_cost = go_costs[node] + come_costs[node]
-        best_cost = max(best_cost, current_cost)
-
-    return best_cost
+def solution():
+    dijkstra(x, rev_dir)
+    dijkstra(x, directed)
+    max_cost = max(costs[1:])
+    print(max_cost)
 
 
-def dijkstra_go(start: int, end: int):
-    min_heap = [(0, start, start)]
-    costs = [0] * (n + 1)
-    recents = [0] * (n + 1)
+def dijkstra(start: int, connected: List[List[int]]):
+    min_heap = [(0, start)]
     is_visited = [False] * (n + 1)
     while len(min_heap):
-        cost, recent, current = heapq.heappop(min_heap)
+        cost, current = heapq.heappop(min_heap)
 
         if is_visited[current]:
             continue
 
-        costs[current] = cost
-        recents[current] = recent
         is_visited[current] = True
-        if current == end:
-            go_costs[start] = cost
-            break
-
-        for idx, val in enumerate(connected[current]):
-            heapq.heappush(min_heap, (cost + val[0], current, val[1]))
-    recent = recents[end]
-    while recent != start:
-        if go_costs[recent] == DEFAULT:
-            go_costs[recent] = go_costs[end] - costs[recent]
-        recent = recents[recent]
-
-
-def dijkstra_come(start: int):
-    min_heap = [(0, start)]
-    while len(min_heap):
-        cost, current = heapq.heappop(min_heap)
-        if come_costs[current] != DEFAULT:
-            continue
-
-        come_costs[current] = cost
+        costs[current] += cost
 
         for idx, val in enumerate(connected[current]):
             heapq.heappush(min_heap, (cost + val[0], val[1]))
@@ -67,11 +31,12 @@ def dijkstra_come(start: int):
 
 if __name__ == '__main__':
     n, m, x = map(int, input().split())
-    connected = [[] for node in range(n + 1)]
+    directed = [[] for node in range(n + 1)]
+    rev_dir = [[] for node in range(n + 1)]
     for edge in range(m):
-        depart, arrive, t = map(int, input().split())
-        connected[depart].append((t, arrive))
-    go_costs = [DEFAULT] * (n + 1)
-    come_costs = [DEFAULT] * (n + 1)
-    sol = solution()
-    print(sol)
+        start, end, t = map(int, input().split())
+        directed[start].append((t, end))
+        rev_dir[end].append((t, start))
+
+    costs = [0] * (n + 1)
+    solution()
