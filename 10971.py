d931a57c0e214303b7f059082505c3bbcb62b486
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-16 21:49:43 +0900

    10971.1.2
    AC in Python 3
    
    10971.1.1에서의 변경점
    1. 탐색값이 기존의 최적값보다 더 클 경우 탐색을 중단하고 다음 가지로 넘어간다
    2. 백트래킹을 하나의 노드에서만 시작한다. <= 이유: 해밀턴 사이클을 구하는 문제이므로, 출발 지점이 어디든 최적값은 동일하게 된다.

--- 10971.py
+++ 10971.py
@@ -2,37 +2,37 @@ import sys
 
 
 input = sys.stdin.readline
-MAX_COST = 1_000_000 * 10
+MAX_COST = 10_000_000
+HOMETOWN = 0
 
 
 def solution():
-    global min_cost, hometown
+    global min_cost
     min_cost = MAX_COST
-    for node in range(n):
-        hometown = node
-        is_visited[node] = True
-        backtrack(node, 1, 0)   # 현재 위치, 방문 노드 개수, 현재까지의 비용
-        is_visited[node] = False
-
-    print(min_cost)
+    is_visited[HOMETOWN] = True
+    backtrack(HOMETOWN, 1, 0)
+    return min_cost
 
 
 def backtrack(current_node: int, num_visit: int, current_cost: int):
-    if num_visit == n:
-        global min_cost, hometown
-        if graph[current_node][hometown]:
-            min_cost = min(min_cost, current_cost + graph[current_node][hometown])
+    global min_cost
+    if current_cost > min_cost:
+        return
+
+    if num_visit == n and dist[current_node][HOMETOWN]:
+        min_cost = min(min_cost, current_cost + dist[current_node][HOMETOWN])
         return
 
     for next_node in range(n):
-        if (not is_visited[next_node]) and (graph[current_node][next_node]):
+        if (not is_visited[next_node]) and dist[current_node][next_node]:
             is_visited[next_node] = True
-            backtrack(next_node, num_visit + 1, current_cost + graph[current_node][next_node])
+            backtrack(next_node, num_visit + 1, current_cost + dist[current_node][next_node])
             is_visited[next_node] = False
 
 
 if __name__ == '__main__':
     n = int(input())
-    graph = [list(map(int, input().split())) for col in range(n)]
+    dist = [list(map(int, input().split())) for col in range(n)]
     is_visited = [False] * n
-    solution()
+    sol = solution()
+    print(sol)
