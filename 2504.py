3a554ce5959dd10b56747081f11798bff7e2f34e
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-16 18:10:25 +0900

    2504.1.1
    WA

---
+++ 2504.py
@@ -0,0 +1,43 @@
+import sys
+
+
+input = sys.stdin.readline
+
+pair = {'(': ')',
+        '[': ']'}
+
+
+def solution() -> str:
+    stack = []
+    tmp_val = 0
+    for idx, char in enumerate(inp_str):
+        if char == '(' or char == '[':
+            stack.append((char, tmp_val))
+            tmp_val = 0
+        else:
+            if len(stack) == 0:
+                return '0'
+            stored_char, stored_val = stack.pop()
+            if pair[stored_char] != char:
+                return '0'
+            else:
+                if tmp_val == 0:
+                    if stored_char == '(':
+                        tmp_val = 2
+                    else:
+                        tmp_val = 3
+                else:
+                    if stored_char == '(':
+                        tmp_val *= 2
+                    else:
+                        tmp_val *= 3
+                tmp_val += stored_val
+
+    res = str(tmp_val)
+    return res
+
+
+if __name__ == '__main__':
+    inp_str = input().rstrip()
+    sol = solution()
+    print(sol)
