1cef1bf62828635dbd72a9d7ff76a2f5e7d6ac6f
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-17 12:23:36 +0900

    1043.1.1
    AC
    
    1. 같은 파티에 참석한 사람들을 간선으로 묶는다
    2. bfs을 하여 진실을 듣는 사람과 연결된 모든 사람들에 대해 진실을 듣는다고 처리한다
    3. 진실을 듣는 사람이 아예 없는 파티의 수를 센다.

--- 1043.py
+++ 1043.py
@@ -1,47 +1,72 @@
 import sys
+from itertools import combinations
+from collections import deque
 
 
-N, M = map(int, sys.stdin.readline().split())
-inp = list(map(int, sys.stdin.readline().split()))
-if inp == [0]:
-    cnt_lie = M
-else:
-    cnt_lie = 0
-    num_know, l_know = inp.pop(0), inp
-    l_check = [False for _ in range(N + 1)]
-    for person in l_know:
-        l_check[person] = True
+input = sys.stdin.readline
 
-    ll_come = []
-    for party in range(M):
-        temp = list(map(int, sys.stdin.readline().split()))
-        if temp == [0]:
-            continue
-        num_come, l_come = temp.pop(0), temp
-        ll_come.append(l_come)
 
-    det_loop = True
-    while det_loop:
-        det_loop = False
-        for group in ll_come:
-            heard_truth = False
-            for person in group:
-                if l_check[person]:
-                    heard_truth = True
-                    break
-            if heard_truth:
-                for person in group:
-                    if not l_check[person]:
-                        det_loop = True
-                        l_check[person] = True
+def solution() -> str:
+    for node_a, node_b in combinations(truth_list, 2):
+        connected[node_a].append(node_b)
+        connected[node_b].append(node_a)
 
-    for party in range(M):
-        tell_truth = False
-        for person in ll_come[party]:
-            if l_check[person]:
-                tell_truth = True
+    for idx, party in enumerate(party_list):
+        for node_a, node_b in combinations(party, 2):
+            connected[node_a].append(node_b)
+            connected[node_b].append(node_a)
+
+    if num_truth == 0:
+        return f'{m}'
+
+    bfs(truth_list[0])
+
+    cnt = 0
+    for idx, party in enumerate(party_list):
+        is_fun = True
+        for i, node in enumerate(party):
+            if node in truth_set:
+                is_fun = False
                 break
-        if not tell_truth:
-            cnt_lie += 1
+        if is_fun:
+            cnt += 1
 
-print(cnt_lie)
+    return f'{cnt}'
+
+
+def bfs(node: int):
+    is_visited = [False] * (n + 1)
+    visit_deque = deque([node])
+    is_truth = False
+    while len(visit_deque):
+        current_node = visit_deque.popleft()
+        if is_visited[current_node]:
+            continue
+
+        is_visited[current_node] = True
+
+        if current_node in truth_set:
+            is_truth = True
+
+        for idx, next_node in enumerate(connected[current_node]):
+            visit_deque.append(next_node)
+
+    if is_truth:
+        for node, is_visit in enumerate(is_visited):
+            if is_visit:
+                truth_set.add(node)
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    inp = list(map(int, input().split()))
+    num_truth = inp[0]
+    truth_list = inp[1:]
+    truth_set = set(truth_list)
+    party_list = []
+    for party in range(m):
+        inp = list(map(int, input().split()))
+        party_list.append(inp[1:])
+
+    connected = [[] for node in range(n + 1)]
+    print(solution())
