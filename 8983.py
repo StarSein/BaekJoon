c65007ee05e36c47232897530b65940c9af85d91
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-17 19:40:06 +0900

    8983.2.1
    WA
    
    맞왜틀

--- 8983.py
+++ 8983.py
@@ -6,27 +6,33 @@ input = sys.stdin.readline
 
 def solution() -> str:
     pad_list.sort()
-    target_list.sort()
 
-    cnt_prey = 0
-    i = 0
+    cnt_catch = 0
     for idx, target in enumerate(target_list):
         tx, ty = target
-        px = pad_list[i]
+        px = binary_search(tx)
         if is_in_range(tx, ty, px):
-            cnt_prey += 1
-            continue
+            print(tx, ty, px)
+            cnt_catch += 1
 
-        if i < m - 1:
-            while tx > px:
-                i += 1
+    return f'{cnt_catch}'
 
-                px = pad_list[i]
-                if is_in_range(tx, ty, px):
-                    cnt_prey += 1
-                    break
 
-    return f'{cnt_prey}'
+def binary_search(target_x: int) -> int:
+    start, end = 0, m - 1
+    while start < end - 1:
+        mid = start + (end - start + 1) // 2
+        if target_x > pad_list[mid]:
+            start = mid + 1
+        elif target_x < pad_list[mid]:
+            end = mid - 1
+        else:
+            return pad_list[mid]
+
+    if abs(target_x - pad_list[start]) < abs(target_x - pad_list[end]):
+        return pad_list[start]
+    else:
+        return pad_list[end]
 
 
 def is_in_range(tx: int, ty: int, px: int) -> bool:
