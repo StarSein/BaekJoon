63efbfcc3615e04d5529dbe24d07dced8e1117d2
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-18 20:21:23 +0900

    2473.1.2
    AC in Python 3
    
    2473.1.1 대비 변경점 
    1. 투 포인터의 간격을 줄여가는 로직이 더욱 간소화되었다.
    2. 세 용액의 특성값이 0인 경우를 제외하는 조건문으로 인한 자잘한 시간 소요가 없도록, 바로 함숫값을 리턴한다.

--- 2473.py
+++ 2473.py
@@ -1,62 +1,35 @@
 import sys
 from typing import Tuple
 
+
 input = sys.stdin.readline
+MAX_ABS_SUM = 3_000_000_001
 
 
-def solution() -> Tuple[int]:
-    val_list.sort()
-    lp, rp = 0, n - 1
-    mp = binary_search(lp, rp)
-    best_mix = (val_list[lp], val_list[mp], val_list[rp])
-    best_sum = abs(sum(best_mix))
-    while lp < rp - 2:
-        mp_a = binary_search(lp + 1, rp)
-        mp_b = binary_search(lp, rp - 1)
-        next_mix_a = (val_list[lp + 1], val_list[mp_a], val_list[rp])
-        next_mix_b = (val_list[lp], val_list[mp_b], val_list[rp - 1])
-        next_sum_a = abs(sum(next_mix_a))
-        next_sum_b = abs(sum(next_mix_b))
-        if next_sum_a < next_sum_b:
-            next_sum = next_sum_a
-            next_mix = next_mix_a
-            lp += 1
-        else:
-            next_sum = next_sum_b
-            next_mix = next_mix_b
-            rp -= 1
+def solution() -> Tuple[int, int, int]:
+    num_list.sort()
+    min_abs_sum = MAX_ABS_SUM
+    for lp in range(n - 2):
+        mp, rp = lp + 1, n - 1
+        while mp < rp:
+            cur_sum = num_list[lp] + num_list[mp] + num_list[rp]
+            cur_abs_sum = abs(cur_sum)
+            if cur_abs_sum < min_abs_sum:
+                min_abs_sum = cur_abs_sum
+                best_mix = (num_list[lp], num_list[mp], num_list[rp])
+                if min_abs_sum == 0:
+                    return best_mix
 
-        if next_sum < best_sum:
-            best_sum = next_sum
-            best_mix = next_mix
+            if cur_sum > 0:
+                rp -= 1
+            else:
+                mp += 1
 
     return best_mix
 
 
-def binary_search(lp: int, rp: int) -> int:
-    target = -(val_list[lp] + val_list[rp])
-    start, end = lp + 1, rp - 1
-    while start <= end:
-        mid = start + (end - start) // 2
-        if target < val_list[mid]:
-            end = mid - 1
-        elif target > val_list[mid]:
-            start = mid + 1
-        else:
-            return mid
-
-    candidates = []
-    for dx in range(-1, 2, 1):
-        cdd = mid + dx
-        if lp < cdd < rp:
-            candidates.append((abs(target - val_list[cdd]), cdd))
-    candidates.sort()
-
-    return candidates[0][1]
-
-
 if __name__ == '__main__':
     n = int(input())
-    val_list = list(map(int, input().split()))
+    num_list = list(map(int, input().split()))
 
     print(*solution())
