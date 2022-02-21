4d0b8cd2c1ea7bd47576f23c3ee26c2b4d0c0eed
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-21 18:36:57 +0900

    2042.1.1
    WA
    
    1. 바이너리 인덱스 트리(펜윅 트리) 구현
    2. 트리 생성 - 누적합의 배열을 만들어 두고 이를 바탕으로 트리의 배열을 생성. idx와 0이 아닌 마지막 비트 t에 대해 prefix_sum[idx] - prefix_sum[idx - t]로 트리의 데이터 초기화
    3. 트리의 한 원소 갱신 - [인덱스 += 해당 인덱스의 마지막 비트]를 거듭해가며 데이터 갱신
    4. 트리의 구간합(b, c) 계산 - 구간합(1, x)를 구할 때 [인덱스 -= 해당 인덱스의 마지막 비트]를 거듭해가며 합산. (1, c) 에서 (1, b - 1)을 차감한 값

---
+++ 2042.py
@@ -0,0 +1,58 @@
+import sys
+from typing import List
+
+
+input = sys.stdin.readline
+
+
+def main():
+    n, m, k = map(int, input().split())
+    num_list = list(int(input()) for num in range(n))
+
+    prefix_sums = [0]
+    for idx, num in enumerate(num_list):
+        prefix_sums.append(prefix_sums[-1] + num_list[idx])
+
+    bin_idx_tree = [0]
+    for idx in range(1, n + 1):
+        t = idx & -idx
+        val = prefix_sums[idx] - prefix_sums[idx - t]
+        bin_idx_tree.append(val)
+
+    for query in range(m + k):
+        a, b, c = map(int, input().split())
+        if a == 1:
+            change = c - num_list[b-1]
+            update_bi_tree(b, change, n, bin_idx_tree)
+        elif a == 2:
+            print_interval_sum(b, c, bin_idx_tree)
+        else:
+            pass
+
+
+def update_bi_tree(b: int, change: int, n: int, bin_idx_tree: List[int]):
+    idx = b
+    while idx <= n:
+        bin_idx_tree[idx] += change
+        idx += (idx & -idx)
+
+
+def print_interval_sum(b: int, c: int, bin_idx_tree: List[int]):
+    prefix_sum_c = 0
+    idx = c
+    while idx:
+        prefix_sum_c += bin_idx_tree[idx]
+        idx -= (idx & -idx)
+
+    prefix_sum_b = 0
+    idx = b - 1
+    while idx:
+        prefix_sum_b += bin_idx_tree[idx]
+        idx -= (idx & -idx)
+
+    interval_sum = prefix_sum_c - prefix_sum_b
+    print(f'{interval_sum}')
+
+
+if __name__ == '__main__':
+    main()
