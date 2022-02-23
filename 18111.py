1a8907fcdf1f9250ec37acc86236ec4f91a7c479
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-23 10:54:44 +0900

    18111.2.1
    AC
    
    좌표가 어딘지는 중요하지 않고, 높이값의 개수만 중요한 문제이므로, 2차원 배열에서 계산하는 것이 아닌 딕셔너리(높이 - 개수)를 참조하며 계산하는 것이 훨씬 효율적이다.
    참조할 공간의 개수 O(NM, 최대 25만) -> 최대 257

--- 18111.py
+++ 18111.py
@@ -1,5 +1,6 @@
 import sys
 from typing import List, Tuple
+from collections import defaultdict
 
 
 input = sys.stdin.readline
@@ -7,49 +8,50 @@ MAX_HEIGHT = 256
 MIN_HEIGHT = 0
 INF = 200_000_000
 
+TIME_DIG = 2
+TIME_PILE = 1
+
 
 def binary_search(n: int, m: int, b: int, site: List[List[int]]) -> Tuple[int, int]:
     min_height = MAX_HEIGHT
     max_height = MIN_HEIGHT
 
+    height_dict = defaultdict(int)
     for col in range(n):
         for row in range(m):
-            min_height = min(min_height, site[col][row])
-            max_height = max(max_height, site[col][row])
+            height = site[col][row]
+            min_height = min(min_height, height)
+            max_height = max(max_height, height)
+            height_dict[height] += 1
 
     best_time = INF
     best_height = -1
-    start, end = min_height, max_height
-    while start <= end:
-        mid = start + (end - start) // 2
-        current_time = total_time(mid, n, m, b, site)
+    for current_height in range(min_height, max_height + 1):
+        current_time = total_time(current_height, b, height_dict)
         if current_time == INF:
-            end = mid - 1
-        else:
-            start = mid + 1
+            break
 
         if current_time <= best_time:
             best_time = current_time
-            best_height = mid
+            best_height = current_height
 
     return best_time, best_height
 
 
-def total_time(height: int, n: int, m: int, b: int, site: List[List[int]]) -> int:
+def total_time(target_height: int, b: int, height_dict: dict) -> int:
     num_dig = 0
-    num_stack = 0
-    for col in range(n):
-        for row in range(m):
-            gap = site[col][row] - height
-            if gap > 0:
-                num_dig += gap
-            else:
-                num_stack += -gap
+    num_pile = 0
+    for init_height, cnt in height_dict.items():
+        gap = target_height - init_height
+        if gap > 0:
+            num_pile += gap * cnt
+        else:
+            num_dig -= gap * cnt
 
-    if num_stack > b + num_dig:
+    if num_pile > b + num_dig:
         return INF
     else:
-        return num_stack * 1 + num_dig * 2
+        return num_dig * TIME_DIG + num_pile * TIME_PILE
 
 
 def main():
