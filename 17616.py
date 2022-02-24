7df277d8e23d168ed48049f470619cfc668853e8
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-02-24 12:10:24 +0900

    17616.2.1
    AC
    
    17616.1.1 의 경우 위상정렬을 이용해 풀이하였으나,
    결국 x의 순위 최솟값을 구하기 위해서는 도착하기까지의 노드들을 역추적해야 하므로, 위상정렬을 쓸 이유가 없다.
    
    1. U 구하기: 정방향 그래프
    2. V 구하기: 역방향 그래프
    3. x의 선행 노드들을 bfs로 탐색한다

--- 17616.py
+++ 17616.py
@@ -8,37 +8,31 @@ def input():
 
 def main():
     n, m, x = map(int, input().split())
-    precede_nums = [0] * (n + 1)
-    followers = [[] for node in range(n + 1)]
-    rev_precede_nums = [0] * (n + 1)
-    rev_followers = [[] for node in range(n + 1)]
+    div_edges = [[] for node in range(n + 1)]
+    rev_edges = [[] for node in range(n + 1)]
     for info in range(m):
         a, b = map(int, input().split())
-        precede_nums[b] += 1
-        followers[a].append(b)
-        rev_precede_nums[a] += 1
-        rev_followers[b].append(a)
-    u = topological_sort(x, precede_nums, followers)
-    v = n + 1 - topological_sort(x, rev_precede_nums, rev_followers)
+        div_edges[b].append(a)
+        rev_edges[a].append(b)
+    u = bfs(x, div_edges)
+    v = n + 1 - bfs(x, rev_edges)
     print(u, v)
 
 
-def topological_sort(x, precede_nums, followers) -> int:
-    precede_nums[0] = -1
-    queue = deque([node for node, num in enumerate(precede_nums) if num == 0])
-    rank = 1
-    if x in queue:
-        return rank
+def bfs(node, edges) -> int:
+    queue = deque([node])
+    is_visiting = [False] * (len(edges))
+    rank = 0
     while len(queue):
         current_node = queue.pop()
-        num_superior = len(queue)
-        for inferior_node in followers[current_node]:
-            precede_nums[inferior_node] -= 1
-            if precede_nums[inferior_node] == 0:
-                if inferior_node == x:
-                    return rank + num_superior + 1
-                queue.append(inferior_node)
+        for superior_node in edges[current_node]:
+            if is_visiting[superior_node]:
+                continue
+
+            queue.append(superior_node)
+            is_visiting[superior_node] = True
         rank += 1
+    return rank
 
 
 if __name__ == '__main__':
