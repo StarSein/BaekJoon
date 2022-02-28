29fd4252b63a8b0176d63ec5da0df542393f7497
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-03-01 00:14:35 +0900

    5719.1.3
    RE (KeyError)
    
    5719.1.2 개선 : 각 노드까지의 최단 경로들을 저장하고, 그것과 길이가 같은 최단 경로들도 저장했다.
    
    line 44~46에서 이미 제거된 간선을 중복으로 처리하게 되다 보니, 존재하지 않는 key값에 접근하게 되는 오류가 발생했다.

--- 5719.py
+++ 5719.py
@@ -1,7 +1,9 @@
 import sys
 import heapq
+from collections import deque
 
 
+INF = int(10e7)
 NONE = -1
 
 
@@ -12,27 +14,36 @@ def input():
 def main():
     def dijkstra(start: int, end: int):
         heap = [(0, NONE, start)]
-        is_visited = [False] * n
+        min_route = [INF] * n
         while len(heap):
             current_cost, recent_node, current_node = heapq.heappop(heap)
-            if is_visited[current_node]:
-                continue
+            if current_cost > min_route[current_node]:
+                if current_node == end:
+                    return min_route[current_node]
+                else:
+                    continue
 
-            recent_list[current_node] = recent_node
-            if current_node == end:
-                return current_cost
-            is_visited[current_node] = True
+            min_route[current_node] = current_cost
+            recent_list[current_node].add(recent_node)
 
             for next_node, weight in connected[current_node].items():
                 heapq.heappush(heap, (current_cost + weight, current_node, next_node))
-        return -1
+        if min_route[end] == INF:
+            return -1
+        else:
+            return min_route[end]
 
     def erase_short_path():
-        current_node = d
-        while current_node != s:
-            recent_node = recent_list[current_node]
-            connected[recent_node].pop(current_node)
-            current_node = recent_node
+        queue = deque([d])
+        while len(queue):
+            current_node = queue.popleft()
+
+            if current_node == s:
+                continue
+
+            for recent_node in recent_list[current_node]:
+                connected[recent_node].pop(current_node)
+                queue.append(recent_node)
 
     while True:
         n, m = map(int, input().split())
@@ -43,20 +54,16 @@ def main():
         for road in range(m):
             u, v, p = map(int, input().split())
             connected[u][v] = p
-        recent_list = [NONE] * n
+        recent_list = [set() for node in range(n)]
         best_route = dijkstra(s, d)
         if best_route == -1:
             print(best_route)
             continue
-            
+
         erase_short_path()
-        while True:
-            recent_list = [NONE] * n
-            second_route = dijkstra(s, d)
-            if second_route > best_route or second_route == -1:
-                print(second_route)
-                break
-            erase_short_path()
+
+        second_route = dijkstra(s, d)
+        print(second_route)
 
 
 main()
