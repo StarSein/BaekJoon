328a2a0379d0df2ea3d19d5b5043b5682782b7ba
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-03-02 00:24:02 +0900

    20159.1.2
    AC
    
    20159.1.1 개선: 밑장 빼기의 시점에 밑장과 윗장 중 최적의 선택을 반영한다.

--- 20159.py
+++ 20159.py
@@ -9,17 +9,21 @@ def main():
     n = int(input())
     x_list = list(map(int, input().split()))
 
-    prefix_sum = [0]
+    prefix_even_sum = [0]
     for even_idx in range(0, n, 2):
-        prefix_sum.append(prefix_sum[-1] + x_list[even_idx])
-    suffix_sum = [0] * (n // 2 + 1)
+        prefix_even_sum.append(prefix_even_sum[-1] + x_list[even_idx])
+    suffix_odd_sum = [0] * (n // 2 + 1)
     idx = -2
     for odd_idx in range(n - 1, 0, -2):
-        suffix_sum[idx] = suffix_sum[idx+1] + x_list[odd_idx]
+        suffix_odd_sum[idx] = suffix_odd_sum[idx+1] + x_list[odd_idx]
         idx -= 1
+
     max_sum = 0
     for i in range(n // 2 + 1):
-        max_sum = max(max_sum, prefix_sum[i] + suffix_sum[i])
+        if x_list[2*i-1] <= x_list[-1]:
+            max_sum = max(max_sum, prefix_even_sum[i] + suffix_odd_sum[i])
+        else:
+            max_sum = max(max_sum, prefix_even_sum[i] + suffix_odd_sum[i] + x_list[2*i-1] - x_list[-1])
 
     print(max_sum)
 
