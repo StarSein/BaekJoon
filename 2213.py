c174d0119aec63d4182ec808d18e576f78778028
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-03-02 18:45:16 +0900

    2213.1.1
    WA
    
    join 메소드를 사용하기 위해 노드 번호를 문자열의 형태로 저장했는데,
    이걸 정렬하면 오름차순이 아닌, 사전 순 정렬이 된다는 것을 간과했다.

---
+++ 2213.py
@@ -0,0 +1,52 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def main():
+    def init_dp(current_node: int, parent_node: int):
+        connected[current_node].discard(parent_node)
+        for child_node in connected[current_node]:
+            init_dp(child_node, current_node)
+
+        if connected[current_node]:
+            dp[current_node][0] = w_list[current_node] + sum(dp[child_node][1] for child_node in connected[current_node])
+            dp[current_node][1] = sum(max(dp[child_node]) for child_node in connected[current_node])
+        else:
+            dp[current_node][0] = w_list[current_node]
+            dp[current_node][1] = 0
+
+    def det_contain(current_node: int):
+        if dp[current_node][0] >= dp[current_node][1]:
+            contained_set.add(str(current_node))
+            for child in connected[current_node]:
+                for grand_child in connected[child]:
+                    det_contain(grand_child)
+        else:
+            for child in connected[current_node]:
+                det_contain(child)
+
+    HEAD = 0
+    ROOT = 1
+
+    n = int(input())
+    w_list = list(map(int, input().split()))
+    w_list.insert(0, HEAD)
+    connected = [set() for node in range(n + 1)]
+    for edge in range(n - 1):
+        a, b = map(int, input().split())
+        connected[a].add(b)
+        connected[b].add(a)
+
+    dp = [[-1, -1] for node in range(n + 1)]
+    init_dp(ROOT, HEAD)
+    print(max(dp[ROOT]))
+    contained_set = set()
+    det_contain(ROOT)
+    print(' '.join(sorted(list(contained_set))))
+
+
+if __name__ == '__main__':
+    main()
