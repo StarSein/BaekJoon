4c33c702e2edf1d468d2a8ead530314a019088e9
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-03-06 13:48:25 +0900

    14863.1.2
    TLE in Python 3
    AC in PyPy3
    
    1. 배낭 문제와 같은 원리, DP를 구현한 풀이
    2. 일반적인 배낭 문제와 다른 점: 배낭 문제의 경우 i번째 배낭을 포함시킬지 말지 여부를 고르는 것, 이 문제의 경우 i번째 구간에서 자전거와 도보 둘 중 하나는 선택을 해야 하는 것.
    
    14863.1.1 대비 개선점: dp를 INF라는 변수로 초기화하여, 자전거와 도보 중 아무것도 이용하지 않은 경우는 최댓값 계산에서 제외되도록 하였다. 단 dp[0]는 line 23~26의 범용적 적용을 위해 만들어진 더미 배열이므로 0으로 초기화하여 최댓값 계산에 사용한다.

--- 14863.py
+++ 14863.py
@@ -6,6 +6,7 @@ def input():
 
 
 def main():
+    INF = -int(1e9)
     TIME = 0
     FUND = 1
     n, k = map(int, input().split())
@@ -17,17 +18,12 @@ def main():
         bike.append((bike_time, bike_fund))
     dp = [[0 for row in range(k + 1)] for col in range(n + 1)]
     for cur in range(1, n + 1):
-        for time_limit in range(1, k + 1):
-            if walk[cur][TIME] > time_limit and bike[cur][TIME] > time_limit:
-                dp[cur][time_limit] = dp[cur-1][time_limit]
-                continue
-
-            cur_walk, cur_bike = 0, 0
+        for time_limit in range(0, k + 1):
+            dp[cur][time_limit] = INF
             if walk[cur][TIME] <= time_limit:
-                cur_walk = max(dp[cur-1][time_limit], dp[cur-1][time_limit - walk[cur][TIME]] + walk[cur][FUND])
+                dp[cur][time_limit] = max(dp[cur][time_limit], dp[cur-1][time_limit - walk[cur][TIME]] + walk[cur][FUND])
             if bike[cur][TIME] <= time_limit:
-                cur_bike = max(dp[cur-1][time_limit], dp[cur-1][time_limit - bike[cur][TIME]] + bike[cur][FUND])
-            dp[cur][time_limit] = max(cur_walk, cur_bike)
+                dp[cur][time_limit] = max(dp[cur][time_limit], dp[cur-1][time_limit - bike[cur][TIME]] + bike[cur][FUND])
     print(dp[n][k])
 
 
