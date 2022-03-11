9b89d479122f4978af983fcdeec2d2c0d298821f
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-03-11 12:53:19 +0900

    9466.1.1
    TLE in Python 3, PyPy3
    
    시간복잡도: O(N^2)
    
    1. is_contained 배열 선언. 초기값 -1.
    2. is_contained[stud] == -1인 학생에 대해서만 dfs 시행
    3. dfs 함수 내부에 visited_set, visited_stack 선언. visited_set에 들어있는 노드에 방문할 경우, 탐색을 중단하고, 해당 노드가 pop될 때까지 visited_stack를 pop하면서 is_contained[node] = 1로 한다. 남아있는 visited_stack 의 노드들에 대해서는 is_contained[node] = 0

---
+++ 9466.py
@@ -0,0 +1,47 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def main():
+    NOT_CHECKED = -1
+
+    def dfs(start_node: int):
+        visited_stack = []
+        visited_set = set()
+
+        curr_node = start_node
+        while curr_node not in visited_set:
+            visited_stack.append(curr_node)
+            visited_set.add(curr_node)
+            curr_node = choice_list[curr_node-1]
+        cycle_end = curr_node
+
+        while True:
+            curr_node = visited_stack.pop()
+            is_contained[curr_node] = True
+            if curr_node == cycle_end:
+                break
+
+        while len(visited_stack):
+            curr_node = visited_stack.pop()
+            is_contained[curr_node] = False
+
+    t = int(input())
+    for tc in range(t):
+        n = int(input())
+        choice_list = list(map(int, input().split()))
+
+        is_contained = [NOT_CHECKED] * (n + 1)
+        for stud in range(1, n + 1):
+            if is_contained[stud] == NOT_CHECKED:
+                dfs(stud)
+
+        answer = n - (sum(is_contained) + 1)
+        print(answer)
+
+
+if __name__ == '__main__':
+    main()
