e915d9dde2571a0d7af4cf13b1f74130971fee5c
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-03-21 22:13:53 +0900

    2293.1.2
    AC
    
    2293.1.1 대비 개선점: 2차원 배열을 1차원 배열로 축소해 메모리 소모를 줄였다.
    2차원 배열에서 row에 생긴 변화는 그것보다 작지 않은 row에만 파급이 되므로, 
    row가 하던 기능을 for 문으로 대체할 수 있다

--- 2293.py
+++ 2293.py
@@ -7,16 +7,14 @@ def input():
 
 def main():
     n, k = map(int, input().split())
-    coin_vals = [0] + [int(input()) for i in range(n)]
-    coin_vals.sort()
-
-    dp = [[0 for coin_val in range(n + 1)] for sum_val in range(k + 1)]
-    dp[0][0] = 1
-    for col in range(1, k + 1):
-        for row in range(1, n + 1):
-            dp[col][row] = sum(dp[col - coin_vals[row]][i] for i in range(row + 1))
-
-    print(sum(dp[k]))
+    coin_vals = [int(input()) for i in range(n)]
+    dp = [0] * (k + 1)
+    dp[0] = 1
+    for coin_val in coin_vals:
+        for sum_val in range(1, k + 1):
+            if sum_val >= coin_val:
+                dp[sum_val] += dp[sum_val - coin_val]
+    print(dp[k])
 
 
 if __name__ == '__main__':
