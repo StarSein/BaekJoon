e7a73e09f7c7f11dcc44de15281a53d5a0599393
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-03-31 20:55:16 +0900

    19535.1.2
    AC
    
    19535.1.1 대비 개선점: 재귀 호출을 이용한 전위 순회 방식이 아닌, deque를 이용한 층별 순회 방식으로 트리를 만들어 시간 소모를 줄였다.
    스택을 이용하면 전위 순회 방식도 가능하다.

--- 19535.py
+++ 19535.py
@@ -1,7 +1,7 @@
 """
 트리의 노드별 정보를 바탕으로 계산할 수 있다.
 depth[node], num_child[node], parent[node] 를 사용한다.
-전위 순회 방식으로 트리를 만들고 depth, num_child 배열을 완성한다
+하향식 층별 순회 방식으로 트리를 만들고 depth, num_child 배열을 완성한다
 ㄷ의 개수: depth >= 3인 모든 노드에 대해, 자식 노드의 개수를 합한 것
          depth와 관계 없이 모든 노드에 대해, 자식 노드의 개수 * (부모 노드의 자식 수 - 1)을 합한 것
 ㅈ의 개수: depth >= 2인 모든 노드에 대해, comb(자식 노드의 개수, 2)를 합한 것
@@ -11,8 +11,7 @@ depth[node], num_child[node], parent[node] 를 사용한다.
 
 import sys
 from math import comb
-
-sys.setrecursionlimit(int(3e5 + 50))
+from collections import deque
 
 
 def input():
@@ -31,20 +30,24 @@ def main():
     num_child = [-1] * (n + 1)
     parent = [-1] * (n + 1)
 
-    def make_tree(curr_node: int):
-        depth[curr_node] = depth[parent[curr_node]] + 1
-        num_child[curr_node] = len(graph[curr_node])
-        for next_node in graph[curr_node]:
-            if next_node == parent[curr_node]:
-                num_child[curr_node] -= 1
-            else:
-                parent[next_node] = curr_node
-                make_tree(next_node)
+    def make_tree():
+        dq = deque([ROOT])
+        while dq:
+            curr_node = dq.popleft()
+            parent_node = parent[curr_node]
+            depth[curr_node] = depth[parent_node] + 1
+            num_child[curr_node] = len(graph[curr_node])
+            for next_node in graph[curr_node]:
+                if next_node == parent_node:
+                    num_child[curr_node] -= 1
+                else:
+                    parent[next_node] = curr_node
+                    dq.append(next_node)
 
     ROOT = 1
     depth[0] = 0
     parent[ROOT] = 0
-    make_tree(ROOT)
+    make_tree()
 
     def count_d() -> int:
         cnt = 0
