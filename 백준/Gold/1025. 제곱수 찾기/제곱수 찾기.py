d3876e4ba8e0ed74b1ad7f3bdf7d18b6192e183d
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-09-26 19:54:02 +0900

    [Gold V] Title: 제곱수 찾기, Time: 300 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.py
+++ 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.py
@@ -18,7 +18,6 @@ O(N^4) = 9^4 = 3^8 = 6561
     정답: 4
     출력: -1
 원인) 수열의 길이가 1일 경우 행 공차와 열 공차 모두 0이어도 된다는 점을 간과함
-해결) 따로 조건 분기
 
 [try 2] WA
 반례)
@@ -30,9 +29,18 @@ O(N^4) = 9^4 = 3^8 = 6561
     정답: 64
     출력: 16
 원인) 수열의 길이를 '인덱스 범위를 벗어나지 않는 최대한'으로 고정했던 점.
-해결) 수열의 길이도 1~9까지 다양하게 설정. 따라서 시간 복잡도는 O(N^5)
 
+[개선] 수열의 길이가 긴 것부터 만들어보고,
+결과가 나왔으면 그것보다 작은 길이의 제곱수는 만들 필요가 없다.
+
+[try3] WA
+원인) sequence의 길이가 원래 의도한 길이와 같을 때에만 유의미한 값으로 취급했는데,
+    [0, 1, 2, 3] 처럼 앞에 0이 오는 수열의 길이도 4로 취급한 점이 오류.
+    
+[try4] WA
+원인) 문자열 '0'를 sequence에 넣어놓고 정수 0 과 비교함.
 """
+
 from sys import stdin
 from math import sqrt
 
@@ -45,7 +53,10 @@ def solution() -> int:
     table = [list(map(lambda x: int(x), iter(input()))) for _ in range(N)]
     answer = -1
 
-    for seq_len in range(1, MAX_LEN + 1):
+    for seq_len in range(MAX_LEN, 0, -1):
+        if answer != -1:
+            break
+
         for r_start in range(N):
             for c_start in range(M):
                 for r_step in range(-N + 1, N, 1):
@@ -60,6 +71,10 @@ def solution() -> int:
                                 sequence.append(str(table[row][col]))
                             else:
                                 break
+
+                        if (seq_len > 1 and sequence[0] == '0') or len(sequence) != seq_len:
+                            continue
+
                         result = int(''.join(sequence))
                         if sqrt(result).is_integer():
                             answer = max(answer, result)
