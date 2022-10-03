3d2fd09f40633f12779bd4c3a50def7a5506a2ca
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-10-03 17:49:23 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 1460 ms, Memory: 113036 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.py
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.py
@@ -1,11 +1,13 @@
 """
-1. 'SAFE ZONE'을 어디 설치하는지는 중요치 않다. 최소 개수가 중요하다.
-2. 'SAFE ZONE'의 최소 개수는 연결 요소의 개수와 같다.
-3. 1000 X 1000 의 각 인덱스를 하나의 노드로 간주하면 O(N^2)에 풀이 가능하다.
-4. 다만 구현의 편의를 위해 단방향 간선을 양방향 간선으로 취급하자.
+1. 분리 집합으로 다 묶어준다.
+2. 분리 집합의 개수를 센다.
+
+try 1) WA.
+원인: roots 배열을 갱신하지 않았음에도 find 메소드를 통하지 않고 
+    직접 roots 배열을 통해 분리집합의 개수를 센 것
+해결: union-find 를 한 다음, find 메소드를 통해 분리집합의 개수를 세기
 """
 from sys import stdin
-from collections import deque
 from typing import List
 
 
@@ -14,41 +16,40 @@ input = lambda: stdin.readline().rstrip()
 
 def solution(N: int, M: int, grid: List[List[str]]) -> int:
     NUM_NODE = N * M
-    graph = [[] for _ in range(NUM_NODE)]
 
     dir_dict = {'R': (0, 1),
                 'D': (1, 0),
                 'L': (0, -1),
                 'U': (-1, 0)}
 
+    roots = [i for i in range(NUM_NODE)]
+
+    def find_root(node: int) -> int:
+        if roots[node] == node:
+            return node
+        else:
+            roots[node] = find_root(roots[node])
+            return roots[node]
+
+    def union(rootA: int, rootB: int):
+        if rootA > rootB:
+            rootA, rootB = rootB, rootA
+
+        roots[rootB] = rootA
+
     for r in range(N):
         for c in range(M):
             dr, dc = dir_dict[grid[r][c]]
             nr, nc = r + dr, c + dc
-            if 0 <= nr < N and 0 <= nc < M:
-                nodeA = r * M + c
-                nodeB = nr * M + nc
-                graph[nodeA].append(nodeB)
-                graph[nodeB].append(nodeA)
+            cur_node = r * M + c
+            next_node = nr * M + nc
 
-    def find_component(_node: int):
-        dq = deque()
-        dq.append(node)
-        visited[node] = True
-        while dq:
-            cur_node = dq.popleft()
-            for next_node in graph[cur_node]:
-                if not visited[next_node]:
-                    dq.append(next_node)
-                    visited[next_node] = True
+            root_cur = find_root(cur_node)
+            root_next = find_root(next_node)
+            if root_cur != root_next:
+                union(root_cur, root_next)
 
-    visited = [False] * NUM_NODE
-    component_cnt = 0
-    for node in range(NUM_NODE):
-        if not visited[node]:
-            find_component(node)
-            component_cnt += 1
-
+    component_cnt = len(set((find_root(node) for node in range(NUM_NODE))))
     return component_cnt
 
 
@@ -56,4 +57,3 @@ if __name__ == '__main__':
     _N, _M = map(int, input().split())
     _grid = [list(input()) for _ in range(_N)]
     print(solution(_N, _M, _grid))
-    
