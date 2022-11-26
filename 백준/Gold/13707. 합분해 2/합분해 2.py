d8c34b84b492e56807f3b5dc6e168c1181037be1
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2022-11-26 23:18:11 +0900

    [Gold IV] Title: 합분해 2, Time: 88 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Gold/13707. 합분해 2/합분해 2.py
+++ 백준/Gold/13707. 합분해 2/합분해 2.py
@@ -1,4 +1,5 @@
 from sys import stdin
+from math import comb
 
 
 def input():
@@ -7,15 +8,9 @@ def input():
 
 def solution(n: int, k: int) -> int:
     MOD = int(1e9)
-
-    dp = [1 for _ in range(n + 1)]
-    for _ in range(k - 1):
-        for i in range(1, n + 1):
-            dp[i] = (dp[i] + dp[i - 1]) % MOD
-    return dp[n] % MOD
+    return comb(n + k - 1, n) % MOD
 
 
 if __name__ == '__main__':
     N, K = map(int, input().split())
     print(solution(N, K))
-    
