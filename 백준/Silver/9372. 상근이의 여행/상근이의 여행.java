aa2846ce850499b279ca0560d42ddda9b47ecbd4
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2023-02-12 17:55:42 +0900

    [Silver IV] Title: 상근이의 여행, Time: 232 ms, Memory: 24352 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < M; j++) {
+                br.readLine();
+            }
+            answer.append(N - 1).append('\n');
+        }
+        System.out.println(answer);
+    }
+}
