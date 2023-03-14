9e5a061cd7bdaa8fa417870aa83d294aeee33ef7
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2023-03-14 22:15:33 +0900

    [Silver I] Title: 귀여운 라이언, Time: 424 ms, Memory: 70772 KB -BaekjoonHub

---
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, K;
+    static boolean[] isRyan;
+
+    public static void main(String[] args) throws Exception {
+        readInput();
+        solve();
+    }
+
+    static void readInput() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        isRyan = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            isRyan[i] = st.nextToken().equals("1");
+        }
+    }
+
+    static void solve() {
+        int answer = Integer.MAX_VALUE;
+        int lp = 0, rp = 1;
+        int ryanCnt = (isRyan[0] ? 1 : 0);
+        while (lp < N) {
+            if (ryanCnt >= K) {
+                answer = Math.min(answer, rp - lp);
+                if (isRyan[lp++]) ryanCnt--;
+            } else if (rp < N) {
+                if (isRyan[rp++]) ryanCnt++;
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
+    }
+}
