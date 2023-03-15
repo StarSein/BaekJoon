c2619afe3c0cd4f13979df524351d5b767ab8519
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2023-03-15 21:46:48 +0900

    [Gold III] Title: 색상환, Time: 356 ms, Memory: 57820 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.py
+++ 백준/Gold/2482. 색상환/색상환.py
@@ -5,7 +5,7 @@ def input():
     return stdin.readline().rstrip()
 
 
-def read_test_case():
+def read_input():
     N = int(input())
     K = int(input())
     return N, K
@@ -13,16 +13,18 @@ def read_test_case():
 
 def solution(N: int, K: int) -> int:
     MOD = int(1e9 + 3)
-    dp = [[[0, 0] for j in range(K + 1)] for i in range(N + 1)]
-    for i in range(N + 1):
-        dp[i][0][0] = 1
-    for i in range(1, N + 1):
-        for j in range(1, K + 1):
-            dp[i][j][0] = (dp[i - 1][j][1] + dp[i - 1][j][0]) % MOD
-            dp[i][j][1] = dp[i - 1][j - 1][0]
-    answer = (dp[N - 1][K][1] + dp[N][K][0]) % MOD
-    return answer
+
+    dp = [[[0 for k in range(K + 1)] for j in range(N + 1)] for i in range(2)]
+    dp[0][0][0] = 1
+    dp[0][1][0] = 1
+    dp[1][1][1] = 1
+    for i in range(2):
+        for j in range(2, N + 1):
+            dp[i][j][0] = dp[i][j - 1][0]
+            for k in range(1, K + 1):
+                dp[i][j][k] = (dp[i][j - 1][k] + dp[i][j - 2][k - 1]) % MOD
+    return (dp[0][N][K] + dp[1][N - 1][K]) % MOD
 
 
 if __name__ == '__main__':
-    print(solution(*read_test_case()))
+    print(solution(*read_input()))
