15b5fcb4e6e5256f2221f669c92edf639e97f7eb
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2023-03-20 22:02:48 +0900

    [Gold IV] Title: 토너먼트 만들기, Time: 136 ms, Memory: 38892 KB -BaekjoonHub

--- 백준/Gold/2262. 토너먼트 만들기/토너먼트 만들기.py
+++ 백준/Gold/2262. 토너먼트 만들기/토너먼트 만들기.py
@@ -13,36 +13,23 @@ def read_input():
 
 
 def solution(n: int, ranks: List[int]) -> int:
-    dp = [[[0, 0] for j in range(n)] for i in range(n)]
-
+    answer = 0
     for i in range(n):
-        dp[i][i][1] = ranks[i]
-
-    for i in range(n - 1):
-        dp[i][i + 1][0] = abs(ranks[i] - ranks[i + 1])
-        dp[i][i + 1][1] = min(ranks[i], ranks[i + 1])
-
-    def func(l: int, r: int) -> List[int]:
-        if dp[l][r][1] != 0:
-            return dp[l][r]
-
-        min_diff_sum = 1000000
-        min_rank = 10000
-        for k in range(l, r):
-            diff_sum1, rank1 = func(l, k)
-            diff_sum2, rank2 = func(k + 1, r)
-            cur_diff_sum = diff_sum1 + diff_sum2 + abs(rank1 - rank2)
-            cur_rank = min(rank1, rank2)
-            if cur_diff_sum < min_diff_sum:
-                min_diff_sum = cur_diff_sum
-                min_rank = cur_rank
-            elif cur_diff_sum == min_diff_sum and cur_rank < min_rank:
-                min_rank = cur_rank
-        dp[l][r][0] = min_diff_sum
-        dp[l][r][1] = min_rank
-        return dp[l][r]
-
-    return func(0, n - 1)[0]
+        cur_rank = ranks[i]
+        if cur_rank == 1:
+            continue
+            
+        diff = 10000
+        for j in range(i - 1, -1, -1):
+            if ranks[j] < cur_rank:
+                diff = min(diff, cur_rank - ranks[j])
+                break
+        for j in range(i + 1, n, 1):
+            if ranks[j] < cur_rank:
+                diff = min(diff, cur_rank - ranks[j])
+                break
+        answer += diff
+    return answer
 
 
 if __name__ == '__main__':
