6f0e953f1eaedf73f72cab985d21c7dfb9b8c6e7
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2023-03-22 21:54:20 +0900

    [Gold IV] Title: 타일 채우기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.py
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -6,23 +6,24 @@ def input():
 
 
 def solution() -> int:
-    COL = 1 << 3
-    dp = [[0 for i in range(COL)] for j in range(N + 2)]
-    dp[0][0] = 1
-    for i in range(N):
-        dp[i][(COL - 1) ^ (1 << 2)] += dp[i][0]
-        dp[i][(COL - 1) ^ (1 << 0)] += dp[i][0]
-        dp[i + 2][0] += dp[i][0]
+    dp = [[0 for j in range(8)] for i in range(N + 2)]
+    dp[0][1 | 2 | 4] = 1
+    for i in range(1, N + 1):
+        dp[i][1 | 2] += dp[i - 1][1 | 2 | 4]
+        dp[i][2 | 4] += dp[i - 1][1 | 2 | 4]
+        dp[i + 1][1 | 2 | 4] += dp[i - 1][1 | 2 | 4]
 
-        dp[i + 1][0] += dp[i][1 << 0]
-        dp[i + 1][0] += dp[i][1 << 2]
-        dp[i + 1][(COL - 1) ^ (1 << 0)] += dp[i][1 << 0]
-        dp[i + 1][(COL - 1) ^ (1 << 2)] += dp[i][1 << 2]
+        dp[i][1 | 2 | 4] += dp[i][1]
+        dp[i + 1][2 | 4] += dp[i][1]
 
-        dp[i + 1][1 << 2] += dp[i][(COL - 1) ^ (1 << 2)]
-        dp[i + 1][1 << 0] += dp[i][(COL - 1) ^ (1 << 0)]
+        dp[i][1 | 2 | 4] += dp[i][4]
+        dp[i + 1][1 | 2] += dp[i][4]
 
-    return dp[N][0]
+        dp[i + 1][4] += dp[i][1 | 2]
+
+        dp[i + 1][1] += dp[i][2 | 4]
+
+    return dp[N][1 | 2 | 4]
 
 
 if __name__ == '__main__':
