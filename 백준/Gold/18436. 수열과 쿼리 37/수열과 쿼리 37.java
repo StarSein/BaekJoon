cf586db3a08eeb034cc0e1e5da099659190ac58a
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2023-06-06 03:31:26 +0900

    [Gold I] Title: 수열과 쿼리 37, Time: 692 ms, Memory: 61692 KB -BaekjoonHub

--- 백준/Gold/18436. 수열과 쿼리 37/수열과 쿼리 37.java
+++ 백준/Gold/18436. 수열과 쿼리 37/수열과 쿼리 37.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
 
     static int N, M;
-    static int[] A, seg;
+    static int[] A;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,12 +12,8 @@ public class Main {
         A = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-        }
-
-        seg = new int[4 * N];
-        for (int i = 1; i <= N; i++) {
-            update(1, 1, N, i, A[i]);
+            int num = Integer.parseInt(st.nextToken());
+            update(i, num & 1);
         }
 
         M = Integer.parseInt(br.readLine());
@@ -27,46 +23,44 @@ public class Main {
             int type = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int res;
+            int prev, val, res;
             switch (type) {
                 case 1:
-                    update(1, 1, N, a, b);
+                    prev = queryRange(a, a);
+                    if ((prev + (b & 1)) == 1) {
+                        val = (prev == 1 ? -1 : 1);
+                        update(a, val);
+                    }
                     break;
                 case 2:
-                    res = (b - a + 1) - query(1, 1, N, a, b);
+                    res = (b - a + 1) - queryRange(a, b);
                     sb.append(res).append('\n');
                     break;
                 case 3:
-                    res = query(1, 1, N, a, b);
+                    res = queryRange(a, b);
                     sb.append(res).append('\n');
             }
         }
         System.out.print(sb);
     }
 
-    static void update(int node, int start, int end, int target, int value) {
-        if (target < start || target > end) {
-            return;
+    static void update(int i, int x) {
+        while (i <= N) {
+            A[i] += x;
+            i += i & -i;
         }
-        if (start == target && target == end) {
-            seg[node] = value & 1;
-            return;
-        }
-        int mid = (start + end) >> 1;
-        update(node << 1, start, mid, target, value);
-        update(node << 1 | 1, mid + 1, end, target, value);
-        seg[node] = seg[node << 1] + seg[node << 1 | 1];
     }
 
-    static int query(int node, int start, int end, int left, int right) {
-        if (right < start || left > end) {
-            return 0;
+    static int queryRange(int l, int r) {
+        return query(r) - query(l - 1);
+    }
+
+    static int query(int i) {
+        int ret = 0;
+        while (i > 0) {
+            ret += A[i];
+            i -= i & -i;
         }
-        if (left <= start && end <= right) {
-            return seg[node];
-        }
-        int mid = (start + end) >> 1;
-        return query(node << 1, start, mid, left, right)
-                + query(node << 1 | 1, mid + 1, end, left, right);
+        return ret;
     }
 }
