262b5c322d8abd6feb8962d62fbf1749a4581c27
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2023-06-11 21:54:21 +0900

    [Gold II] Title: 나누기, Time: 228 ms, Memory: 21108 KB -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.java
+++ 백준/Gold/21757. 나누기/나누기.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class Main {
 
     static int N;
-    static int[] A, dp;
+    static long answer;
+    static int[] A;
+    static long[] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,16 +20,26 @@ public class Main {
         for (int i = 1; i < N; i++) {
             A[i] += A[i - 1];
         }
-        int sum = A[N - 1];
-        int itvSum = sum / 4;
-        dp = new int[5];
-        dp[0] = 1;
-        for (int prefSum : A) {
-            if (prefSum >= itvSum && (prefSum % itvSum) == 0) {
-                int k = prefSum / itvSum;
-                dp[k] += dp[k - 1];
+        int itvSum = A[N - 1] / 4;
+        if (itvSum == 0) {
+            long zeroCnt = 0L;
+            for (int i = 0; i < N - 1; i++) {
+                if (A[i] == 0L) zeroCnt++;
             }
+            answer = zeroCnt * (zeroCnt - 1) * (zeroCnt - 2) / 6;
+        } else {
+            dp = new long[4];
+            dp[0] = 1L;
+            for (int i = 0; i < N - 1; i++) {
+                for (int k = 1; k <= 3; k++) {
+                    if (A[i] == k * itvSum) {
+                        dp[k] += dp[k - 1];
+                        break;
+                    }
+                }
+            }
+            answer = dp[3];
         }
-        System.out.println(dp[4]);
+        System.out.println(answer);
     }
 }
