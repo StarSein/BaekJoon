69e40e5f5a0e9f9b51e5fe60a45d32fd919050e8
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2023-07-30 18:45:06 +0900

    [Gold V] Title: 끝나지 않는 파티, Time: 460 ms, Memory: 26240 KB -BaekjoonHub

---
+++ 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    static int N, M;
+    static int[][] dist;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        dist = new int[N + 1][N + 1];
+        for (int r = 1; r <= N; r++) {
+            st = new StringTokenizer(br.readLine());
+            for (int c = 1; c <= N; c++) {
+                dist[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            sb.append(dist[a][b] <= c ? "Enjoy other party\n" : "Stay here\n");
+        }
+        System.out.println(sb);
+    }
+}
