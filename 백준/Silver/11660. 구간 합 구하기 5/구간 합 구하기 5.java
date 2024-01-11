897388e6f8766970889eae88f0d9f95097ee408a
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2024-01-12 06:27:37 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 836 ms, Memory: 118496 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] map = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				map[i][j] += map[i][j - 1];
+			}
+		}
+		
+		for (int j = 1; j <= N; j++) {
+			for (int i = 1; i <= N; i++) {
+				map[i][j] += map[i - 1][j];
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		int x1, y1, x2, y2;
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			x1 = Integer.parseInt(st.nextToken());
+			y1 = Integer.parseInt(st.nextToken());
+			x2 = Integer.parseInt(st.nextToken());
+			y2 = Integer.parseInt(st.nextToken());
+			sb.append(map[x2][y2] - map[x2][y1 - 1] - map[x1 - 1][y2] + map[x1 - 1][y1 - 1]);
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+}
