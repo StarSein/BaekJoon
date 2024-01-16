89ea29e0deffd9a3aa4ec25e676e023da050d52f
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2024-01-17 03:00:49 +0900

    [Gold II] Title: 카누 선수, Time: 3112 ms, Memory: 124624 KB -BaekjoonHub

--- 백준/Gold/9007. 카누 선수/카누 선수.java
+++ 백준/Gold/9007. 카누 선수/카누 선수.java
@@ -1,10 +1,3 @@
-// A = c1 + c2, B = k - c3 - c4 라고 하면
-// (c1 + c2 + c3 + c4) 의 값이 k 에 근접한다는 것은
-// (A - B) 의 값이 0 에 근접하다는 것과 동치이다
-// 즉 이 문제는 A와 가장 가까운 B를 찾는 것과 같다.
-// 단, A가 고정되어 있다고 할 때 B는 가능한 한 최댓값이어야 한다
-
-
 import java.io.*;
 import java.util.*;
 
@@ -14,7 +7,6 @@ public class Main {
     static int T, k, n;
     static int[][] c;
     static int[] A, B;
-    static ArrayList<Integer> bList;
 
     public static void main(String[] args) throws Exception {
         StringBuilder sb = new StringBuilder();
@@ -34,7 +26,7 @@ public class Main {
                 }
             }
 
-            // A: (c1 + c2), B: (k - c3 - c4)를 저장하는 배열을 만든다
+            // A: (c1 + c2), B: (c3 + c4)를 저장하는 배열을 만든다
             int size = n * n;
             A = new int[size];
             int ai = 0;
@@ -43,41 +35,36 @@ public class Main {
                     A[ai++] = c1 + c2;
                 }
             }
-            bList = new ArrayList<>(size);
+            B = new int[size];
+            int bi = 0;
             for (int c3 : c[2]) {
                 for (int c4 : c[3]) {
-                    bList.add(k - c3 - c4);
+                    B[bi++] = c3 + c4;
                 }
             }
 
-            // B는 오름차순 정렬되고 중복이 없는 배열로 만든다
-            B = bList.stream().sorted().distinct().mapToInt(Integer::intValue).toArray();
+            // A, B를 오름차순 정렬한다
+            Arrays.sort(A);
+            Arrays.sort(B);
 
-            // A의 모든 원소를 순회하면서 B에서 해당 원소 이상의 값 중 최솟값의 인덱스 t를 찾는다
+            // A의 최솟값, B의 최댓값에서 시작하여 투 포인터를 이용하여 합이 k에 가까운 합을 갱신한다
             int minAbsDiff = Integer.MAX_VALUE;
             int bestSum = Integer.MAX_VALUE;
-            for (int a : A) {
-                int start = 0;
-                int end = B.length - 1;
-                int t = end;
-                while (start <= end) {
-                    int mid = (start + end) / 2;
-                    if (B[mid] >= a) {
-                        t = mid;
-                        end = mid - 1;
-                    } else {
-                        start = mid + 1;
-                    }
+            ai = 0;
+            bi = B.length - 1;
+            while (ai < A.length && bi >= 0) {
+                int curSum = A[ai] + B[bi];
+                int curAbsDiff = Math.abs(curSum - k);
+                if (curAbsDiff < minAbsDiff || curAbsDiff == minAbsDiff && curSum < bestSum) {
+                    minAbsDiff = curAbsDiff;
+                    bestSum = curSum;
                 }
-
-                // t 뿐만 아니라 t-1 또한 후보가 될 수 있다. 이를 반영하여 정답을 갱신한다
-                for (int i = 0; i < 2 && t >= 0; i++, t--) {
-                    int curAbsDiff = Math.abs(a - B[t]);
-                    int curSum = a - B[t] + k;
-                    if (curAbsDiff < minAbsDiff || (curAbsDiff == minAbsDiff && curSum < bestSum)) {
-                        minAbsDiff = curAbsDiff;
-                        bestSum = curSum;
-                    }
+                if (curSum < k) {
+                    ai++;
+                } else if (curSum > k) {
+                    bi--;
+                } else {
+                    break;
                 }
             }
 
