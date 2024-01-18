c2f8982f757f58539c8c571a3de1140ab8815efb
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2024-01-19 05:02:31 +0900

    [Gold IV] Title: 부분합, Time: 288 ms, Memory: 23864 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -19,33 +19,26 @@ public class Main {
             seq[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 구간합이 S 이상이 될 때까지 구간의 오른쪽 끝점을 우측으로 움직인다
-        int s = 0;
-        int e = 0;
-        int partialSum = 0;
-        for (; e < N && partialSum < S; e++) {
-            partialSum += seq[e];
-        }
-
-        // S 이상의 구간합을 만들 수 없는 경우 0을 출력한다
-        if (partialSum < S) {
-            System.out.println(0);
-            return;
-        }
-
         // 부분합에 해당하는 구간의 두 포인터 s, e 를 우측으로 움직이며 정답을 갱신한다
-        int answer = N;
-        partialSum -= seq[--e];
-        for (; e < N; e++) {
+        int answer = Integer.MAX_VALUE;
+        int s = 0;
+        int partialSum = 0;
+        for (int e = 0; e < N; e++) {
             partialSum += seq[e];
+
             // 구간합이 S 미만이 되지 않는 선에서 s를 우측으로 움직인다
             while (partialSum - seq[s] >= S) {
                 partialSum -= seq[s++];
+            }
+
+            // 부분합이 S 이상일 때에만 정답을 갱신한다
+            if (partialSum < S) {
+                continue;
             }
             answer = Math.min(answer, e - s + 1);
         }
 
         // 정답을 출력한다
-        System.out.println(answer);
+        System.out.println(answer == Integer.MAX_VALUE ? 0 : answer);
     }
 }
