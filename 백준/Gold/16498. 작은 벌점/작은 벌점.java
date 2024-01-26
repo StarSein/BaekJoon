5105b37cd8cbffa7ac212bd69f3acc4c08426eb3
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2024-01-27 01:20:28 +0900

    [Gold V] Title: 작은 벌점, Time: 164 ms, Memory: 15320 KB -BaekjoonHub

--- 백준/Gold/16498. 작은 벌점/작은 벌점.java
+++ 백준/Gold/16498. 작은 벌점/작은 벌점.java
@@ -1,67 +1,84 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 
-    static int A, B, C, answer;
-    static int[] ai, bi, ci;
+    static final int INF = 1_000_000_000;
+    static int[] cardNums = new int[3];
+    static int[][] cards = new int[3][];
 
     public static void main(String[] args) throws Exception {
+        // 입력을 받는다
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        A = Integer.parseInt(st.nextToken());
-        B = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-
-        ai = new int[A];
-        bi = new int[B];
-        ci = new int[C];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < A; i++) {
-            ai[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < 3; i++) {
+            cardNums[i] = Integer.parseInt(st.nextToken());
         }
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < B; i++) {
-            bi[i] = Integer.parseInt(st.nextToken());
-        }
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < C; i++) {
-            ci[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            int curCardNum = cardNums[i];
+            cards[i] = new int[curCardNum];
+            int[] curCards = cards[i];
+            for (int j = 0; j < curCardNum; j++) {
+                curCards[j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        answer = Integer.MAX_VALUE;
-        procedure(ai, bi, ci);
-        procedure(bi, ci, ai);
-        procedure(ci, ai, bi);
+        // 세 카드들을 오름차순으로 정렬한다
+        for (int i = 0; i < 3; i++) {
+            Arrays.sort(cards[i]);
+        }
+
+        // (a <= b <= c), (b <= a <= c), ..., (c <= b <= a) 의 6가지 경우에 대해 완전 탐색을 한다
+        // 최댓값에 해당하는 카드를 낼 플레이어를 임의로 지정하고,
+        // 다른 두 플레이어 각각에 대해 그 값보다 작은 최댓값을 이분 탐색으로 찾고 정답을 갱신한다
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            int[] higherCards = cards[i];
+            for (int j = 0; j < higherCards.length; j++) {
+                int higherVal = higherCards[j];
+
+                ArrayList<Integer> maxLowerVals = new ArrayList<>();
+                for (int k = 0; k < 3; k++) {
+                    if (i == k) {
+                        continue;
+                    }
+                    int maxLowerVal = getMaxLowerVal(higherVal, cards[k]);
+                    if (maxLowerVal == INF) {
+                        continue;
+                    }
+                    maxLowerVals.add(maxLowerVal);
+                }
+
+                if (maxLowerVals.size() < 2) {
+                    continue;
+                }
+
+                int lowerVal = Math.min(maxLowerVals.get(0), maxLowerVals.get(1));
+
+                answer = Math.min(answer, higherVal - lowerVal);
+            }
+        }
+
+        // 정답을 출력한다
         System.out.println(answer);
     }
 
-    static void procedure(int[] xi, int[] yi, int[] zi) {
-        for (int x : xi) {
-            updateAnswer(x, yi, zi);
+    static int getMaxLowerVal(int x, int[] arr) {
+        if (arr[0] > x) {
+            return INF;
         }
-    }
-
-    static void updateAnswer(int minVal, int[] pi, int[] qi) {
-        int minP = Integer.MAX_VALUE;
-        for (int p : pi) {
-            if (minVal <= p && p < minP) {
-                minP = p;
+        int s = 0;
+        int e = arr.length - 1;
+        while (s <= e) {
+            int mid = (s + e) >> 1;
+            if (arr[mid] <= x) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
             }
         }
-        if (minP == Integer.MAX_VALUE) {
-            return;
-        }
-        int minQ = Integer.MAX_VALUE;
-        for (int q : qi) {
-            if (minVal <= q && q < minQ) {
-                minQ = q;
-            }
-        }
-        if (minQ == Integer.MAX_VALUE) {
-            return;
-        }
-        int maxVal = Math.max(minP, minQ);
-        answer = Math.min(answer, maxVal - minVal);
+        return arr[e];
     }
 }
