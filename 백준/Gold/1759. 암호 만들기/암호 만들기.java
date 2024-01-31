331053ad285ab235e048c59067d711b0aec955f7
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2024-02-01 03:27:10 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,55 +1,60 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
+
 
 public class Main {
 
     static int L, C;
-    static char[] cArr, tgt;
-    static Set<Character> vowelSet = new HashSet<>();
-    static StringBuilder answer = new StringBuilder();
+    static char[] chars, pwd;
+    static boolean[] isVowel;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
-        readInput();
-        solve();
-    }
-
-    static void readInput() throws Exception {
+        // 입력을 받는다
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         L = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        cArr = new char[C];
-        tgt = new char[L];
+        chars = new char[C];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < C; i++) {
-            cArr[i] = st.nextToken().charAt(0);
+            chars[i] = st.nextToken().charAt(0);
         }
-    }
 
-    static void solve() {
-        vowelSet.add('a');
-        vowelSet.add('e');
-        vowelSet.add('i');
-        vowelSet.add('o');
-        vowelSet.add('u');
-        Arrays.sort(cArr);
+        // 문자들을 사전 순으로 정렬한다
+        Arrays.sort(chars);
 
+        // 각 인덱스마다 해당 문자가 모음인지 여부를 미리 저장해 놓는다
+        isVowel = new boolean[C];
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        for (int i = 0; i < C; i++) {
+            char c = chars[i];
+            for (char vowel : vowels) {
+                if (c == vowel) {
+                    isVowel[i] = true;
+                    break;
+                }
+            }
+        }
+
+        // comb(C, L)의 모든 경우의 수에 대해 모음과 자음의 개수 조건을 만족할 경우 정답 문자열에 추가한다
+        pwd = new char[L];
         comb(0, 0, 0);
-        System.out.print(answer);
+
+        // 정답 문자열을 출력한다
+        System.out.print(sb);
     }
 
-    static void comb(int tgtIdx, int start, int vowelCnt) {
-        if (tgtIdx == L) {
-            if (vowelCnt >= 1 && L - vowelCnt >= 2) {
-                answer.append(new String(tgt)).append('\n');
+    static void comb(int pwdIdx, int startIdx, int vowelCount) {
+        if (pwdIdx == L) {
+            if (vowelCount >= 1 && pwdIdx - vowelCount >= 2) {
+                sb.append(pwd).append('\n');
             }
             return;
         }
-
-        for (int i = start; i < C; i++) {
-            tgt[tgtIdx] = cArr[i];
-            comb(tgtIdx + 1, i + 1, vowelSet.contains(cArr[i]) ? vowelCnt + 1 : vowelCnt);
+        for (int i = startIdx; i < C; i++) {
+            pwd[pwdIdx] = chars[i];
+            comb(pwdIdx + 1, i + 1, vowelCount + (isVowel[i] ? 1 : 0));
         }
     }
 }
