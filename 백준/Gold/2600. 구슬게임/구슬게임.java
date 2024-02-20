58174bc690c6b0e1e8c08b97171c22f582bb6a21
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2024-02-21 05:31:00 +0900

    [Gold IV] Title: 구슬게임, Time: 168 ms, Memory: 18164 KB -BaekjoonHub

--- 백준/Gold/2600. 구슬게임/구슬게임.java
+++ 백준/Gold/2600. 구슬게임/구슬게임.java
@@ -4,52 +4,46 @@ import java.util.*;
 
 public class Main {
 
-    static int[] b = new int[3];
-    static int[] k = new int[2];
-    static int[][][] dp;
+    static int[] b;
+    static int k1, k2;
+    static boolean[][] dp, checked;
 
     public static void main(String[] args) throws Exception {
+        // 테케에 공통 적용될 조건을 입력 받는다
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 3; i++) {
-            b[i] = Integer.parseInt(st.nextToken());
-        }
+        b = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        // (1, 1) ~ (500, 500) 에 대해 첫 턴에 시작하는 사람의 승리 여부를 저장해 놓는다
+        dp = new boolean[501][501];
+        checked = new boolean[501][501];
+        win(500, 500);
+
+        // 입력이 주어질 때마다 승자를 정답 문자열에 추가한다
         StringBuilder sb = new StringBuilder();
-        // 모든 테스트케이스에 대해
         for (int tc = 0; tc < 5; tc++) {
-            // 입력을 받는다
-            st = new StringTokenizer(br.readLine());
-            k[0] = Integer.parseInt(st.nextToken());
-            k[1] = Integer.parseInt(st.nextToken());
-
-            dp = new int[2][k[0] + 1][k[1] + 1];
-
-            // 승자를 정답 문자열에 추가한다
-            sb.append(getDP(0, k[0], k[1]) == 1 ? "A\n" : "B\n");
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            k1 = Integer.parseInt(st.nextToken());
+            k2 = Integer.parseInt(st.nextToken());
+            sb.append(win(k1, k2) ? "A\n" : "B\n");
         }
 
         // 정답 문자열을 출력한다
         System.out.print(sb);
     }
 
-    // dp[i][j][k] : 플레이어 i 가 두 구슬 통 속에 남은 구슬 수가 (k1, k2)일 때 이길 수 있는지 여부
-    // 값이 1이면 승리, -1이면 패배, 0이면 아직 탐색되지 않음
-    static int getDP(int player, int k1, int k2) {
-        if (dp[player][k1][k2] != 0) {
-            return dp[player][k1][k2];
+    static boolean win(int i, int j) {
+        if (i < 0 || j < 0) {
+            return true;
         }
-        if (k1 < b[0] && k2 < b[0]) {
-            return dp[player][k1][k2] = -1;
+        if (checked[i][j]) {
+            return dp[i][j];
         }
-        int ret = -1;
-        for (int b_i : b) {
-            if (b_i <= k1) {
-                ret = Math.max(ret, -1 * getDP(1 - player, k1 - b_i, k2));
-            }
-            if (b_i <= k2) {
-                ret = Math.max(ret, -1 * getDP(1 - player, k1, k2 - b_i));
-            }
+        boolean winnable = false;
+        for (int e : b) {
+            winnable |= !win(i - e, j);
+            winnable |= !win(i, j - e);
         }
-        return dp[player][k1][k2] = ret;
+        checked[i][j] = checked[j][i] = true;
+        return dp[i][j] = dp[j][i] = winnable;
     }
 }
