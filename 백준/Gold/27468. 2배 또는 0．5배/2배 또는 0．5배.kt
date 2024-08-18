667132ea6b24a6926b24f3f3a005d6cbedd6556c
Yuseong Hwang <75326335+StarSein@users.noreply.github.com>
2024-08-18 15:38:04 +0900

    [Gold V] Title: 2배 또는 0.5배, Time: 592 ms, Memory: 331428 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/27468. 2배 또는 0．5배/2배 또는 0．5배.kt
+++ 백준/Gold/27468. 2배 또는 0．5배/2배 또는 0．5배.kt
@@ -21,13 +21,14 @@
  * 1 3 2 4 5 7 6 8
  *
  * i) N % 4 == 0
- *      (4n+1) (4n+3) (4n+2) (4n+4) 의 반복만으로 가능
+ *      (a) (4n+1) (4n+3) (4n+2) (4n+4) 의 반복만으로 가능
+ *      (b) (4n+1) (4n+2) (4n+4) (4n+3) 의 반복만으로 가능
  * ii) N % 4 == 1
- *      i)의 마지막에 (N)만 붙이면 가능
+ *      (a)의 마지막에 (N) 만 붙이면 가능
  * iii) N % 4 == 2
- *      i)의 마지막에 (N-1) (N)만 붙이면 가능
+ *      (b)의 마지막에 (N-1) (N) 만 붙이면 가능
  * iv) N % 4 == 3
- *      i)의 마지막에 (N-2) (N) (N-1)만 붙이면 가능
+ *      (a)의 마지막에 (N-2) (N) (N-1) 만 붙이면 가능
  */
 
 fun main() = with(System.`in`.bufferedReader()) {
@@ -39,29 +40,27 @@ fun main() = with(System.`in`.bufferedReader()) {
     sb.append("YES\n")
 
     // N을 4로 나눈 나머지에 맞게 수열을 생성하면서 정답 문자열에 추가한다
-    repeat(N / 4) {
-        sb.append("${4 * it + 1} ")
-        sb.append("${4 * it + 3} ")
-        sb.append("${4 * it + 2} ")
-        sb.append("${4 * it + 4} ")
-    }
-
-    when (N % 4) {
-        0 -> {
-
-        }
-        1 -> {
-            sb.append(N)
-        }
+    val patterns = when (N % 4) {
         2 -> {
-            sb.append("${N - 1} ${N}")
+            arrayOf(1, 2, 4, 3)
         }
         else -> {
-            sb.append("${N - 2} ${N} ${N - 1}")
+            arrayOf(1, 3, 2, 4)
         }
     }
+    val prefix = when (N % 4) {
+        1 -> "$N"
+        2 -> "${N - 1} $N"
+        3 -> "${N - 2} $N ${N - 1}"
+        else -> ""
+    }
+    repeat(N / 4) { i ->
+        repeat(4) { j ->
+            sb.append("${4 * i + patterns[j]} ")
+        }
+    }
+    sb.append(prefix)
 
     // 정답 문자열을 출력한다
     println(sb)
 }
-
